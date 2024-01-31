3b53d9bf0920633bf0a2be13fe989613f10f7367
nijuy <youjin6325@naver.com>
2024-01-31 17:58:52 +0900

    feat: 서랍장 관련 route에 Layout 컴포넌트 적용

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { Drawer } from './drawer/Drawer';
+import { Layout as DrawerLayout } from './drawer/components/Layout/Layout';
 import { Home } from './home/Home';
 import { Search } from './search/Search';
 
@@ -8,7 +9,9 @@ export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/drawer" element={<Drawer />} />
+      <Route path="/drawer" element={<DrawerLayout />}>
+        <Route path="" element={<Drawer />} />
+      </Route>
       <Route path="/search" element={<Search />} />
     </Routes>
   );
