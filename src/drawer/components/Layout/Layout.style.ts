1b54e4aadde36df55144bed0f376512d7c1d6f04
nijuy <youjin6325@naver.com>
2024-02-02 15:19:33 +0900

    design: StyledLayout min-height, padding-bottom 지정
    - 80px은 Footer의 height와 같음

--- src/drawer/components/Layout/Layout.style.ts
+++ src/drawer/components/Layout/Layout.style.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const StyledLayout = styled.div`
   width: 100%;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   position: relative;
+  padding-bottom: 80px;
 `;
