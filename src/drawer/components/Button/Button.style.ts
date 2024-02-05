88b779c996e9da8ead3c36351e45b8713d9788a7
Hanna922 <hanna.urssu@gmail.com>
2024-02-05 18:42:22 +0900

    feat: remove unnecessary type

--- src/drawer/components/Button/Button.style.ts
+++ src/drawer/components/Button/Button.style.ts
@@ -7,11 +7,11 @@ interface StyledButtonProps {
 export const StyledButton = styled.button<StyledButtonProps>`
   padding: 8px 32px;
   border-radius: 9999px;
-  background-color: ${({ $isFilled }: StyledButtonProps) =>
+  background-color: ${({ $isFilled }) =>
     $isFilled ? ({ theme }) => theme.color.buttonPoint : 'transparent'};
-  color: ${({ $isFilled }: StyledButtonProps) =>
+  color: ${({ $isFilled }) =>
     $isFilled ? ({ theme }) => theme.color.bgNormal : ({ theme }) => theme.color.buttonPoint};
-  border: ${({ $isFilled }: StyledButtonProps) =>
+  border: ${({ $isFilled }) =>
     $isFilled ? 'none' : ({ theme }) => `1px solid ${theme.color.buttonPoint}`};
   font-style: ${({ theme }) => theme.typo.caption0};
   &:focus {
