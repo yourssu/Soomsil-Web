d5691923daed0ffa7b3a81890f0e8a9bc78edcec
seocylucky <seocylucky@github.com>
2024-02-07 19:09:11 +0900

    feat: category 리스트 Layout 작업

--- src/drawer/components/Category/Category.style.ts
+++ src/drawer/components/Category/Category.style.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 export const StyledCategoryContainer = styled.div`
+  margin-left: 2.125rem;
+  padding-left: 2.0625rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 1rem;
   font-style: ${({ theme }) => theme.typo.subtitle2};
+  align-self: flex-start;
 `;
