2831ee97a55cdb98f66140656dd97f99a88feacd
seocylucky <seocylucky@github.com>
2024-02-07 19:14:44 +0900

    fix: 카테고리 컴포넌트 margin-top 값 추가

--- src/drawer/components/Category/Category.style.ts
+++ src/drawer/components/Category/Category.style.ts
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
 
 export const StyledCategoryContainer = styled.div`
+  margin-top: 1.3125rem;
   margin-left: 2.125rem;
   padding-left: 2.0625rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  align-self: flex-start;
   gap: 1rem;
   font-style: ${({ theme }) => theme.typo.subtitle2};
-  align-self: flex-start;
 `;
