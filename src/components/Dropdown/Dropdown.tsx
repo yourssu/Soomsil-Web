a0f5cd79bbad6af77fa780bce7e57f578f1b0b3d
JjungminLee <julie0964@naver.com>
2024-02-09 20:35:30 +0900

    fix: dropdown폴더 루트로 이동

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react';
 
 import { Container } from './Dropdown.style';
 
-// JSX를 반환하도록 수정
 export const Dropdown = ({ children }: { children: ReactNode }) => {
   return <Container>{children}</Container>;
 };
