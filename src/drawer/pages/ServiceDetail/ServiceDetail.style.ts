d698fd53dc4d45f1d4777206593aea8b34e5739f
nijuy <youjin6325@naver.com>
2024-02-14 19:03:09 +0900

    feat: `ServiceDetail` 하단 영역 퍼블리싱

--- src/drawer/pages/ServiceDetail/ServiceDetail.style.ts
+++ src/drawer/pages/ServiceDetail/ServiceDetail.style.ts
@@ -20,3 +20,67 @@ export const StyledBackgroundImageContainer = styled.div<StyledBackgroundImageCo
   background-image: url(${(props) => props.$backgroundImage});
   filter: brightness(40%);
 `;
+
+export const StyledDescriptionSection = styled.div`
+  display: grid;
+  gap: 2rem;
+  width: 52.5rem;
+  height: fit-content;
+`;
+
+export const StyledCarousel = styled.div`
+  display: flex;
+  gap: 0.625rem;
+  overflow-x: scroll;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const StyledProductImage = styled.img`
+  width: 27.5rem;
+  height: 18.5rem;
+  border-radius: 0.5rem;
+  margin: 0 auto;
+  flex-shrink: 0;
+  object-fit: cover;
+`;
+
+export const StyledDescriptionPart = styled.div`
+  display: grid;
+  gap: 0.875rem;
+`;
+
+export const StyledSubtitle = styled.p`
+  ${({ theme }) => theme.typo.subtitle1};
+  color: ${({ theme }) => theme.color.textPrimary};
+`;
+
+export const StyledDescription = styled.p`
+  ${({ theme }) => theme.typo.body2};
+  color: ${({ theme }) => theme.color.textSecondary};
+
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+`;
+
+export const StyledMoreProductSection = styled.div`
+  width: 24.8rem;
+  height: fit-content;
+  display: grid;
+  gap: 2rem;
+  padding: 0 0.5rem;
+  margin-left: 5.25rem;
+
+  /* Soomsil/Drawer/Web/title22 */
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 1.375rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 130%; /* 1.7875rem */
+  color: ${({ theme }) => theme.color.textPrimary};
+`;
--- src/drawer/pages/ServiceDetail/ServiceDetail.tsx
+++ src/drawer/pages/ServiceDetail/ServiceDetail.tsx
@@ -1,10 +1,21 @@
+import { IcStarFilled, IconContext } from '@yourssu/design-system-react';
 import { useParams } from 'react-router-dom';
 
 import backgroundImage from '@/assets/service_detail_background.png';
+import { FlexContainer } from '@/components/FlexContainer/FlexContainer';
+import { SmallDrawerCard } from '@/drawer/components/DrawerCard/SmallDrawerCard';
+import { Spacing } from '@/home/components/Spacing/Spacing';
 
 import {
   StyledBackgroundImageContainer,
+  StyledDescription,
+  StyledDescriptionPart,
+  StyledDescriptionSection,
+  StyledMoreProductSection,
+  StyledProductImage,
+  StyledCarousel,
   StyledServiceDetailContainer,
+  StyledSubtitle,
 } from './ServiceDetail.style';
 
 export const ServiceDetail = () => {
@@ -15,6 +26,49 @@ export const ServiceDetail = () => {
       <StyledBackgroundImageContainer $backgroundImage={backgroundImage}>
         {serviceId}
       </StyledBackgroundImageContainer>
+      <Spacing size="5rem" />
+      <FlexContainer>
+        <StyledDescriptionSection>
+          <StyledCarousel>
+            {Array.from({ length: 3 }).map(() => (
+              <StyledProductImage src={backgroundImage} />
+            ))}
+          </StyledCarousel>
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`추천`}</StyledSubtitle>
+            <StyledDescription>
+              {`999`}+
+              <IconContext.Provider value={{ size: '15px', color: '#FDD655' }}>
+                <IcStarFilled />
+              </IconContext.Provider>
+            </StyledDescription>
+          </StyledDescriptionPart>
+
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`서비스 소개`}</StyledSubtitle>
+            <StyledDescription>{`service`}</StyledDescription>
+          </StyledDescriptionPart>
+
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`저작권`}</StyledSubtitle>
+            <StyledDescription>{`Yourssu`}</StyledDescription>
+          </StyledDescriptionPart>
+        </StyledDescriptionSection>
+        <StyledMoreProductSection>
+          {`제작자`}의 서비스 더보기
+          {Array.from({ length: 5 }).map(() => (
+            <SmallDrawerCard
+              link={``}
+              title={`TITLE`}
+              body={`제작자`}
+              bookmarkCount={999}
+              isBookmarked={true}
+            />
+          ))}
+        </StyledMoreProductSection>
+      </FlexContainer>
+
+      <Spacing size="10rem" />
     </StyledServiceDetailContainer>
   );
 };
