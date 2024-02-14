afbc5cb4dff0f7fe3451d5be9d6404bf464bf739
intersoom <sallybig@naver.com>
2024-02-15 03:10:47 +0900

    feat: Logo 클릭시 홈으로 이동

--- src/search/components/SearchBar/SearchBar.tsx
+++ src/search/components/SearchBar/SearchBar.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import logo from '../../assets/Logo.svg';
 import Spacing from '../Spacing';
 
@@ -5,9 +7,16 @@ import { StyledSearchBar } from './SearchBar.style';
 import SearchBox from './SearchBox';
 
 const SearchBar = () => {
+  const navigate = useNavigate();
   return (
     <StyledSearchBar>
-      <img src={logo} alt="logo" />
+      <img
+        src={logo}
+        alt="logo"
+        onClick={() => {
+          navigate('/');
+        }}
+      />
       <Spacing direction="horizontal" size={20} />
       <SearchBox size="large" />
     </StyledSearchBar>
