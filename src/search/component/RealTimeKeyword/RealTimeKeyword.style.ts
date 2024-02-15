bb2da6c16d0f2f0260c68743f601384a826a076c
george <hyunrim03@naver.com>
2024-02-15 10:50:08 +0900

    style: 코드 리뷰 반영

--- src/search/component/RealTimeKeyword/RealTimeKeyword.style.ts
+++ src/search/component/RealTimeKeyword/RealTimeKeyword.style.ts
@@ -3,66 +3,71 @@ import styled from 'styled-components';
 export const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 20px;
-  width: 400px;
-  gap: 10px;
-  border-radius: 12px;
+  padding: 1.25rem;
+  width: 25rem;
+  gap: 0.625rem;
+  border-radius: 0.75rem;
   border: var(--0, 1px) solid var(--border-borderNormal, rgba(16, 17, 18, 0.1));
-  background: var(--sponus_white, #fff);
+  background: ${({ theme }) => theme.color.bgNormal};
 `;
 
 export const StyledHeader = styled.div`
   display: flex;
   position: relative;
-  padding: 8px;
+  padding: 0.5rem;
 `;
 
 export const StyledHeaderTextSection = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 8px;
+  gap: 0.5rem;
 `;
 
-export const StyledHeaderText1 = styled.span`
+export const StyledHeaderTitle = styled.span`
   color: ${({ theme }) => theme.color.textPrimary};
 
   ${({ theme }) => theme.typo.subtitle1};
 `;
 
-export const StyledHeaderText2 = styled.span`
+export const StyledHeaderTime = styled.span`
   color: ${({ theme }) => theme.color.textTertiary};
 
   ${({ theme }) => theme.typo.body2}
 `;
 
 export const StyledHeaderImageSection = styled.img`
-  width: 203px;
-  height: 136px;
+  width: 12.6875rem;
+  height: 8.5rem;
+
   position: absolute;
-  right: -20px;
-  top: -50px;
+  right: -1.25rem;
+  top: -3.125rem;
 `;
 
 export const StyledListItemText = styled.p`
   color: ${({ theme }) => theme.color.textPrimary};
 
-  // body18
-  font-family: Spoqa Han Sans Neo;
-  font-size: 18px;
+  /* PC & Android/body18 */
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 1.125rem;
+  font-style: normal;
   font-weight: 400;
-  line-height: 27px;
-  letter-spacing: 0em;
-  text-align: left;
+  line-height: 130%;
 `;
 
-export const StyledListItemRanking = styled.span`
+interface StyledListItemRankingProp {
+  rankingNumber: number;
+}
+
+export const StyledListItemRanking = styled.span<StyledListItemRankingProp>`
+  color: ${(prop) => (prop.rankingNumber < 4 ? '#8a2ac5' : '#8E9398')};
   font-family: 'Spoqa Han Sans Neo';
-  font-size: 18px;
+  font-size: 1.125rem;
   font-style: normal;
   font-weight: 500;
   line-height: 130%;
-  margin-right: 16px;
+  margin-right: 1rem;
 `;
 
 export const StyledList = styled.div`
