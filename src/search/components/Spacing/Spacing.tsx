713525204291bb299caa5bef3fc288259022d3e2
intersoom <sallybig@naver.com>
2024-02-15 16:18:09 +0900

    refactor: Spacing 컴포넌트 리팩토링

--- src/search/components/Spacing/Spacing.tsx
+++ src/search/components/Spacing/Spacing.tsx
@@ -1,18 +1,14 @@
 import { HTMLAttributes, memo } from 'react';
 
+import { StyledSpacing } from './Spacing.style';
+
 interface SpacingProps extends HTMLAttributes<HTMLDivElement> {
   direction: 'horizontal' | 'vertical';
   size: number;
 }
 
 const Spacing = memo(function Spacing({ direction = 'vertical', size, ...props }: SpacingProps) {
-  return (
-    <div
-      style={direction === 'horizontal' ? { width: `${size}px` } : { height: `${size}px` }}
-      className=""
-      {...props}
-    />
-  );
+  return <StyledSpacing $direction={direction} $size={size} {...props}></StyledSpacing>;
 });
 
 export default Spacing;
