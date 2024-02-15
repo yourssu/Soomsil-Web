d82149bd0e1d500ff94a150a5a07974edae5bd92
고현림 <95522176+indianaPoly@users.noreply.github.com>
2024-02-15 21:16:12 +0900

    feat: Search화면 실시간 검색어 컴포넌트 및 검색 결과 없음 페이지 구현 (#10) (#31)
    * feat: RealTimeKeyword 컴포넌트 제작
    
    * feat: NoResultPage 컴포넌트 제작
    
    * chore: 이미지 파일 위치 변경
    
    * style: YDS 적용
    
    * feat: 시간 앞에 0 붙히기
    
    * chore: 이미지 파일 위치 수정
    
    * style: 코드 리뷰 반영
    
    * refactor: 코드 리뷰 반영
    
    * style: border yds 수정
    
    * style: 코드 리뷰 반영
    
    * refactor: 코드 리뷰 반영
    
    * design: 랭킹 숫자 크기 변경
    
    * refactor: IconContext 적용
    
    * chore: 이미지 위치 수정
    
    ---------
    
    Co-authored-by: george <hyunrim03@naver.com>

---
+++ src/search/component/RealTimeKeyword/RealTimeKeyword.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from 'react';
+
+import { ListItem, IcSearchLine, IconContext } from '@yourssu/design-system-react';
+import { useNavigate } from 'react-router-dom';
+
+import RealTimeKeywordImage from '@/assets/search/realTimeKeyword.png';
+
+import {
+  StyledContainer,
+  StyledHeader,
+  StyledHeaderImageSection,
+  StyledHeaderTitle,
+  StyledHeaderTime,
+  StyledHeaderTextSection,
+  StyledList,
+  StyledListItemRanking,
+  StyledListItemText,
+} from './RealTimeKeyword.style';
+
+interface RealTimeData {
+  year: string;
+  month: string;
+  day: string;
+  hour: string;
+  min: string;
+}
+
+export const RealTimeKeyword = () => {
+  const navigate = useNavigate();
+
+  const [realTime, setRealTime] = useState<RealTimeData>();
+
+  const createTime = () => {
+    const koreaTime = new Date().toLocaleString('en-US', { timeZone: 'Asia/Seoul' });
+    const [date, time] = koreaTime.split(', ');
+    const [month, day, year] = date.split('/');
+    const [hour, min, _] = time.split(':');
+    const [, AMPM] = _.split(' ');
+
+    const setMonth = month.padStart(2, '0');
+    const setDay = day.padStart(2, '0');
+    const setMin = min.padStart(2, '0');
+
+    const setHour =
+      AMPM === 'PM' && Number(hour) !== 12
+        ? String(Number(hour) + 12)
+        : hour.length === 2
+          ? hour
+          : '0' + hour;
+
+    setRealTime({
+      year: year,
+      month: setMonth,
+      day: setDay,
+      hour: setHour,
+      min: setMin,
+    });
+  };
+
+  useEffect(() => {
+    createTime();
+  }, []);
+
+  // api 연동해서 가져와야 합니다. 현재는 임의의 데이터만 넣었습니다.
+  const data = [
+    '숭실대학교',
+    '유어슈',
+    '복학신청',
+    '글로벌미디어학부',
+    '뿌슝이',
+    '로지',
+    '예린',
+    '혀니',
+    '엘리아',
+    '아 복학하기 너무 싫다',
+  ];
+
+  const listOnclick = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const queryKeyword = event.currentTarget.innerText.split('\n')[2];
+    navigate(`?query=${queryKeyword}&page=1`);
+  };
+
+  return (
+    <StyledContainer>
+      <StyledHeader>
+        <StyledHeaderTextSection>
+          <StyledHeaderTitle>
+            숨쉬듯이
+            <br />
+            검색한 키워드
+          </StyledHeaderTitle>
+          {realTime ? (
+            <StyledHeaderTime>
+              {`${realTime.year}.${realTime.month}.${realTime.day} ${realTime.hour}:${realTime.min} 기준`}
+            </StyledHeaderTime>
+          ) : null}
+        </StyledHeaderTextSection>
+        <StyledHeaderImageSection src={RealTimeKeywordImage} alt="뿌슝이" />
+      </StyledHeader>
+      <StyledList>
+        {data.map((value, index) => {
+          return (
+            <ListItem
+              leftIcon={
+                <StyledListItemRanking rankingNumber={index + 1}>{index + 1}</StyledListItemRanking>
+              }
+              /**
+               * @수정사항 color 색상이 YDS에 없어서 HEX로 넣었습니다.
+               */
+              rightIcon={
+                <IconContext.Provider
+                  value={{
+                    color: '#8A2AC5',
+                  }}
+                >
+                  <IcSearchLine />
+                </IconContext.Provider>
+              }
+              key={value}
+              style={{
+                padding: '0.75rem',
+              }}
+              children={<StyledListItemText>{value}</StyledListItemText>}
+              onClick={listOnclick}
+            />
+          );
+        })}
+      </StyledList>
+    </StyledContainer>
+  );
+};
---
+++ src/search/pages/NoResult/NoResult.style.ts
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+  display: flex;
+  w-auto;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2rem;
+  flex-shrink: 0;
+`;
+
+export const StyledNoResultKeyword = styled.span`
+  color: ${({ theme }) => theme.color.textPointed};
+
+  ${({ theme }) => theme.typo.title2}
+`;
+
+export const StyledNoResultDescription = styled.p`
+  color: ${({ theme }) => theme.color.textPrimary};
+
+  /* PC & Android/subtitle24 */
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 1.5rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 130%; /* 1.95rem */
+`;
+
+export const StyledModifySection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`;
+
+export const StyledModifyDescription = styled.p`
+  color: ${({ theme }) => theme.color.textTertiary};
+
+  /* PC & Android/body18 */
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+`;
+
+export const StyledCard = styled.div`
+  display: flex;
+  width: 49.9375rem;
+  height: auto;
+
+  padding: 1.25rem;
+  justify-content: space-between;
+  align-items: center;
+
+  border-radius: 0.75rem;
+  border: 1px solid ${({ theme }) => theme.color.borderNormal};
+  background: ${({ theme }) => theme.color.bgNormal};
+`;
+
+export const StyledCardDescriptionSection = styled.div`
+  display: flex;
+  gap: 1.25rem;
+`;
+
+interface StyledCardIconFrameProp {
+  index: number;
+}
+
+export const StyledCardIconFrame = styled.div<StyledCardIconFrameProp>`
+  display: flex;
+  width: 3.375rem;
+  height: 3.375rem;
+  padding: 0.625rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  background: ${(prop) => (prop.index === 0 ? 'rgba(255, 44, 190, 0.1)' : '#ebe6fb')};
+`;
+
+export const StyledCardTextFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.25rem;
+`;
+
+export const StyledCardLinkText = styled.a`
+  color: ${({ theme }) => theme.color.textPrimary};
+
+  ${({ theme }) => theme.typo.subtitle1}
+
+  &:hover {
+    color: ${({ theme }) => theme.color.textPrimary};
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+export const StyledCardLinkTextDescription = styled.p`
+  color: ${({ theme }) => theme.color.textTertiary};
+
+  /* PC & Android/body16 */
+  font-family: 'Spoqa Han Sans Neo';
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 130%; /* 1.3rem */
+`;
