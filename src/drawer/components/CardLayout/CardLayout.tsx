902e6a23e075459ff5e2dad548c2060c2eb196bb
seocylucky <seocylucky@github.com>
2024-02-19 02:10:47 +0900

    refactor: EmptyScreen 컴포넌트 분리
    + NOT_FOUND_TEXT 상수 대문자 처리

--- src/drawer/components/CardLayout/CardLayout.tsx
+++ src/drawer/components/CardLayout/CardLayout.tsx
@@ -1,46 +1,37 @@
-import { BoxButton } from '@yourssu/design-system-react';
-
-import NotIllust from '@/drawer/assets/noResultDrawer.png';
-
 import { BigDrawerCard } from '../DrawerCard/BigDrawerCard';
 import { UserDrawerCard } from '../DrawerCard/UserDrawerCard/UserDrawerCard';
+import { EmptyScreen } from '../EmptyScreen/EmptyScreen';
 
-import {
-  StyledCardLayoutContainer,
-  StyledNotContainer,
-  StyledNotImg,
-  StyledNotTextBold,
-  StyledNotTextContainer,
-} from './CardLayout.style';
+import { StyledCardLayoutContainer } from './CardLayout.style';
 
 interface CardLayoutProps {
   isDataExist: boolean;
-  type: 'search' | 'star' | 'mydrawer';
+  type: 'SEARCH' | 'STAR' | 'MYDRAWER';
 }
 
-const notFoundText: Record<CardLayoutProps['type'], { boldText: string; subText: string }> = {
-  search: {
+const NOT_FOUND_TEXT: Record<CardLayoutProps['type'], { boldText: string; subText: string }> = {
+  SEARCH: {
     boldText: '해당 검색어에 대한 검색결과가 없습니다.',
     subText: '더 간단한 단어로 검색하거나 직접 서비스를 등록해보세요.',
   },
-  star: {
+  STAR: {
     boldText: '즐겨찾기한 서비스가 없습니다.',
     subText: '내 취향의 서비스를 추가해보세요.',
   },
-  mydrawer: {
+  MYDRAWER: {
     boldText: '내가 등록한 서비스가 없습니다.',
     subText: '새로운 서비스를 등록해보세요.',
   },
 };
 
 export const CardLayout = ({ isDataExist, type }: CardLayoutProps) => {
-  const { boldText, subText } = notFoundText[type];
+  const { boldText, subText } = NOT_FOUND_TEXT[type];
 
   return isDataExist ? (
     <StyledCardLayoutContainer>
       {/* 추후 card id로 map key index 수정 필요 */}
       {Array.from({ length: 9 }).map((_, index) =>
-        type === 'mydrawer' ? (
+        type === 'MYDRAWER' ? (
           <UserDrawerCard
             key={index}
             link={''}
@@ -62,20 +53,6 @@ export const CardLayout = ({ isDataExist, type }: CardLayoutProps) => {
       )}
     </StyledCardLayoutContainer>
   ) : (
-    <StyledNotContainer>
-      <StyledNotTextContainer>
-        <StyledNotTextBold>{boldText}</StyledNotTextBold>
-        <div>{subText}</div>
-      </StyledNotTextContainer>
-      <StyledNotImg src={NotIllust} />
-
-      {type === 'search' || type === 'mydrawer' ? (
-        <BoxButton size="small" variant="filled" rounding={4}>
-          서비스 등록하기
-        </BoxButton>
-      ) : (
-        <></>
-      )}
-    </StyledNotContainer>
+    <EmptyScreen boldText={boldText} subText={subText} type={type} />
   );
 };
