27aa8b2ea4df7bee0748e6836c684ee340e1ca07
nijuy <youjin6325@naver.com>
2024-02-22 01:07:14 +0900

    design: `Button` 컴포넌트 텍스트 정렬 스타일 추가
    - 세로 정렬이 없어서 글자가 살짝 내려가 있었음

--- src/drawer/components/Button/Button.style.ts
+++ src/drawer/components/Button/Button.style.ts
@@ -5,21 +5,28 @@ interface StyledButtonProps {
 }
 
 export const StyledButton = styled.button<StyledButtonProps>`
+  ${({ theme }) => theme.typo.button2};
+
+  display: flex;
+  align-items: center;
+
   height: 2.125rem;
   padding: 0.5rem 2rem;
-  border-radius: 624.9375rem;
+
   background-color: ${({ $isFilled }) =>
     $isFilled ? ({ theme }) => theme.color.buttonPoint : 'transparent'};
   color: ${({ $isFilled }) =>
     $isFilled ? ({ theme }) => theme.color.bgNormal : ({ theme }) => theme.color.buttonPoint};
+
   border: ${({ $isFilled }) =>
     $isFilled ? 'none' : ({ theme }) => `1px solid ${theme.color.buttonPoint}`};
-  ${({ theme }) => theme.typo.button2};
-  cursor: pointer;
+  border-radius: 624.9375rem;
+
   &:hover {
     background-color: ${({ theme }) => theme.color.buttonPointPressed};
     color: ${({ theme }) => theme.color.bgNormal};
   }
+
   &:focus {
     outline: none;
   }
