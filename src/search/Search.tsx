ae40bb71a91f5f5f2b071b8894670b24bffe491f
Hanna922 <hanna.urssu@gmail.com>
2024-02-22 02:38:01 +0900

    remove: remove test code in Search

--- src/search/Search.tsx
+++ src/search/Search.tsx
@@ -1,11 +1,7 @@
-import { SearchBar } from './components/SearchBar/SearchBar';
-import { SearchBox } from './components/SearchBar/SearchBox';
-
 export const Search = () => {
   return (
     <>
-      <SearchBar />
-      <SearchBox size={'small'} />
+      <h1>Search</h1>
     </>
   );
 };
