a7b1ba0f91d8ccb55b03d7104640b5a6b4e70e46
Hanna922 <hanna.urssu@gmail.com>
2024-02-25 21:31:29 +0900

    feat: implement layout in Register

--- src/drawer/pages/Register/Register.style.ts
+++ src/drawer/pages/Register/Register.style.ts
@@ -1,14 +1,53 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
+  @media (max-width: 30rem) {
+    gap: 1.25rem;
+    padding: 1.5rem 0rem;
+  }
+
   display: flex;
   flex-direction: column;
   gap: 2rem;
-  margin-top: 3.37rem;
+  padding: 3.37rem 0rem;
 `;
 
 export const StyledInputContainer = styled.div`
+  @media (max-width: 30rem) {
+    gap: 0.75rem;
+  }
+
   display: flex;
   flex-direction: column;
   gap: 1.56rem;
+`;
+
+export const StyledRightContainer = styled.div`
+  @media (max-width: 30rem) {
+    margin-right: 0rem;
+  }
+
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 1.2rem;
+`;
+
+export const StyledImportText = styled.p`
+  @media (max-width: 30rem) {
+    /* Soomsil/Drawer/Web/body10 */
+    font-size: 'Spoqa Han Sans Neo';
+    font-size: 0.625rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.3;
+  }
+
+  ${({ theme }) => theme.typo.subtitle3};
+  color: ${({ theme }) => theme.color.textPrimary};
+  text-align: right;
+  white-space: pre-wrap;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
--- src/drawer/pages/Register/Register.tsx
+++ src/drawer/pages/Register/Register.tsx
@@ -1,47 +1,80 @@
+import { BoxButton } from '@yourssu/design-system-react';
+
 import { CategoryWithoutAll } from '@/drawer/components/CategoryWithoutAll/CategoryWithoutAll';
 import { Input } from '@/drawer/components/Input/Input';
 import { TextArea } from '@/drawer/components/TextArea/TextArea';
 import { WarningBox } from '@/drawer/components/WarningBox/WarningBox';
+import { MOBILE_VIEW_WIDTH } from '@/drawer/constants/mobileview.constant';
 
-import { StyledContainer, StyledInputContainer } from './Register.style';
+import {
+  StyledContainer,
+  StyledImportText,
+  StyledInputContainer,
+  StyledRightContainer,
+} from './Register.style';
 
 export const Register = () => {
   return (
     <StyledContainer>
       <StyledInputContainer>
-        <Input
-          maxLength={20}
-          title={'서비스 이름'}
-          description={'(최대 20자)'}
-          required={true}
-          isWarned={true}
-        />
+        <StyledRightContainer>
+          <StyledImportText>별표 표시 * 는 필수 입력란</StyledImportText>
+        </StyledRightContainer>
 
-        <Input
-          width={79.625}
-          maxLength={20}
-          title={'간단한 설명'}
-          description={'(최대 20자)'}
-          required={true}
-          disabled={true}
-        />
+        <Input maxLength={20} title={'서비스 이름'} description={'(최대 20자)'} required={true} />
+
+        <Input maxLength={20} title={'간단한 설명'} description={'(최대 20자)'} required={true} />
       </StyledInputContainer>
 
-      <TextArea title="내용" description="(최대 5000자)" required={true} maxLength={5000} />
+      <TextArea maxLength={5000} title={'내용'} description={'(최대 5000자)'} required={true} />
 
       <CategoryWithoutAll />
 
       <StyledInputContainer>
-        <Input maxLength={100} title={'웹 페이지 링크'} description={'(최대 100자)'} />
+        <StyledRightContainer>
+          <StyledImportText>
+            {window.innerWidth < MOBILE_VIEW_WIDTH
+              ? '웹 페이지, Google play, App store, Github\n링크 중 하나는 필수 기재 *'
+              : '웹 페이지, Google play, App store, Github 링크 중 하나는 필수 기재 *'}
+          </StyledImportText>
+        </StyledRightContainer>
 
-        <Input maxLength={100} title={'Google Play 링크'} description={'(최대 100자)'} />
+        <Input
+          maxLength={100}
+          title={'웹 페이지 링크'}
+          description={'(최대 100자)'}
+          validate={(value) => !value.startsWith('https://')}
+        />
 
-        <Input maxLength={100} title={'App Store 링크'} description={'(최대 100자)'} />
+        <Input
+          maxLength={100}
+          title={'Google Play 링크'}
+          description={'(최대 100자)'}
+          validate={(value) => !value.startsWith('https://play.google.com/')}
+        />
 
-        <Input maxLength={100} title={'GitHub 링크'} description={'(최대 100자)'} />
+        <Input
+          maxLength={100}
+          title={'App Store 링크'}
+          description={'(최대 100자)'}
+          validate={(value) => !value.startsWith('https://www.apple.com/')}
+        />
+
+        <Input
+          maxLength={100}
+          title={'GitHub 링크'}
+          description={'(최대 100자)'}
+          validate={(value) => !value.startsWith('https://github.com/')}
+        />
       </StyledInputContainer>
 
       <WarningBox />
+
+      <StyledRightContainer>
+        <BoxButton size={'medium'} variant={'filled'} rounding={4} width="8.125rem">
+          서비스 등록
+        </BoxButton>
+      </StyledRightContainer>
     </StyledContainer>
   );
 };
