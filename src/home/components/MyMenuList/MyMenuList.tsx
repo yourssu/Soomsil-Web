0c37590b72675baa89eeea043ea723f2e2292f85
aaminha <dks4857@gmail.com>
2024-02-28 01:29:42 +0900

    feat: 마이페이지 메뉴 목록 구현

---
+++ src/home/components/MyMenuList/MyMenuList.tsx
@@ -0,0 +1,55 @@
+import { IcArrowRightLine } from '@yourssu/design-system-react';
+import { useTheme } from 'styled-components';
+
+import {
+  StyledContainer,
+  StyledInnerContainer,
+  StyledMenuContainer,
+  StyledMenuListTitle,
+  StyledMenuTitle,
+  StyledListItem,
+} from './MyMenuList.style';
+
+export const MyMenuList = () => {
+  const theme = useTheme();
+
+  return (
+    <StyledContainer>
+      <StyledInnerContainer>
+        <StyledMenuListTitle>마이페이지</StyledMenuListTitle>
+        <StyledMenuContainer>
+          <StyledMenuTitle>계정 관리</StyledMenuTitle>
+          <StyledListItem>비밀번호 변경</StyledListItem>
+          <StyledListItem isPressed>로그아웃</StyledListItem>
+        </StyledMenuContainer>
+        <StyledMenuContainer>
+          <StyledMenuTitle>내 서랍장</StyledMenuTitle>
+          <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            등록한 콘텐츠
+          </StyledListItem>
+          <StyledListItem
+            isPressed
+            rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}
+          >
+            즐겨찾기한 콘텐츠
+          </StyledListItem>
+        </StyledMenuContainer>
+        <StyledMenuContainer>
+          <StyledMenuTitle>웹 정보</StyledMenuTitle>
+          <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            서비스 이용약관
+          </StyledListItem>
+          <StyledListItem
+            isPressed
+            rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}
+          >
+            개인 정보 처리 방침
+          </StyledListItem>
+          <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            오픈소스 사용 정보
+          </StyledListItem>
+        </StyledMenuContainer>
+      </StyledInnerContainer>
+    </StyledContainer>
+  );
+};
