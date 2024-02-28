88b8f2617947d5c092f478010b05b9074e6ed71f
intersoom <sallybig@naver.com>
2024-02-28 15:59:25 +0900

    refactor: 타입 단언 -> 제네릭 활용

--- src/search/hooks/useGetSearch.ts
+++ src/search/hooks/useGetSearch.ts
@@ -1,14 +1,20 @@
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
 
 import { getSearch } from '../apis/getSearch';
 import { GetSearchProps } from '../types/GetSearch.type';
 import { ResultListItemResponse } from '../types/ResultListItem.type';
 
 export const useGetSearch = ({ query }: GetSearchProps) => {
-  return useInfiniteQuery<ResultListItemResponse[]>({
+  return useInfiniteQuery<
+    ResultListItemResponse[],
+    Error,
+    InfiniteData<ResultListItemResponse[]>,
+    string[],
+    number
+  >({
     queryKey: ['getSearch', query],
     queryFn: ({ pageParam }) =>
-      getSearch({ query, page: pageParam as number }).then((data) => data.resultList),
+      getSearch({ query, page: pageParam }).then((data) => data.resultList),
     initialPageParam: 0,
     getNextPageParam: (lastPage, allPages, lastPageParam) => {
       return lastPage !== allPages[-1] ? (lastPageParam as number) + 1 : undefined;
