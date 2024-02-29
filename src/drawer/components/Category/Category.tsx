c5775120e32776eb5cd43f7e33c9d29d47140cd1
seocylucky <seocylucky@github.com>
2024-03-01 02:46:37 +0900

    refactor: 카테고리 id -> category 문자열로 변경

--- src/drawer/components/Category/Category.tsx
+++ src/drawer/components/Category/Category.tsx
@@ -9,27 +9,27 @@ interface IsAllProps {
 }
 
 interface CategoryProps {
-  id: number;
+  category: string;
   title: string;
   subcategories?: string;
 }
 
 const CategoryList: CategoryProps[] = [
-  { id: 1, title: '전체' },
-  { id: 2, title: '교내생활' },
-  { id: 3, title: '취미', subcategories: '(음악, 스포츠, 게임, 여행)' },
-  { id: 4, title: '건강', subcategories: '(건강 및 피트니스, 의료)' },
-  { id: 5, title: 'IT', subcategories: '(AI, 개발자, 디자인)' },
-  { id: 6, title: '지식', subcategories: '(비즈니스, 교육, 뉴스, 금융)' },
-  { id: 7, title: '라이프스타일', subcategories: '(소셜 네트워킹, 미디어, 일상)' },
-  { id: 8, title: '기타' },
+  { category: '', title: '전체' },
+  { category: 'CAMPUS', title: '교내생활' },
+  { category: 'HOBBY', title: '취미', subcategories: '(음악, 스포츠, 게임, 여행)' },
+  { category: 'HEALTH', title: '건강', subcategories: '(건강 및 피트니스, 의료)' },
+  { category: 'IT', title: 'IT', subcategories: '(AI, 개발자, 디자인)' },
+  { category: 'KNOWLEDGE', title: '지식', subcategories: '(비즈니스, 교육, 뉴스, 금융)' },
+  { category: 'LIFESTYLE', title: '라이프스타일', subcategories: '(소셜 네트워킹, 미디어, 일상)' },
+  { category: 'ETC', title: '기타' },
 ];
 
 export const Category = ({ isAll }: IsAllProps) => {
-  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-  const handleCategorySelect = (categoryId: number) => {
-    setSelectedCategory((prev) => (prev === categoryId ? null : categoryId));
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory((prev) => (prev === category ? null : category));
   };
 
   return (
@@ -38,11 +38,11 @@ export const Category = ({ isAll }: IsAllProps) => {
         /* isAll이 true일 때 - 사이드에 위치한 카테고리*/
         <StyledCategoryContainer>
           <div>카테고리 유형</div>
-          {CategoryList.map(({ id, title }) => (
+          {CategoryList.map(({ category, title }) => (
             <CheckBox
-              key={id}
-              isSelected={selectedCategory === id}
-              onChange={() => handleCategorySelect(id)}
+              key={category}
+              isSelected={selectedCategory === category}
+              onChange={() => handleCategorySelect(category)}
             >
               {title}
             </CheckBox>
@@ -51,12 +51,12 @@ export const Category = ({ isAll }: IsAllProps) => {
       ) : (
         /* isAll이 false일 때 - 등록 페이지에 위치한 카테고리*/
         <StyledCategoryWithoutAllContainer>
-          {CategoryList.slice(1).map(({ id, title, subcategories }) => (
+          {CategoryList.slice(1).map(({ category, title, subcategories }) => (
             <CheckBox
               size="medium"
-              key={id}
-              isSelected={selectedCategory === id}
-              onChange={() => handleCategorySelect(id)}
+              key={category}
+              isSelected={selectedCategory === category}
+              onChange={() => handleCategorySelect(category)}
             >{`${title} ${subcategories ?? ''}`}</CheckBox>
           ))}
         </StyledCategoryWithoutAllContainer>
