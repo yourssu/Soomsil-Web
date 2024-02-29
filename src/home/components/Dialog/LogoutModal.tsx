e123671afe7b90a6af395403a5280d9162365cd8
aaminha <dks4857@gmail.com>
2024-02-29 20:36:06 +0900

    feat: 로그아웃 모달창 구현

---
+++ src/home/components/Dialog/LogoutModal.tsx
@@ -0,0 +1,50 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { BoxButton } from '@yourssu/design-system-react';
+
+import PpussungIcon from '@/assets/home/ppussung.svg';
+
+import {
+  StyledButtonContainer,
+  StyledContentInnerContainer,
+  StyledDialogContent,
+  StyledDialogDescription,
+  StyledDialogOverlay,
+  StyledDialogTitle,
+  StyledPpussungIcon,
+} from './LogoutModal.style';
+
+interface LogoutModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const LogoutModal = ({ open, onOpenChange }: LogoutModalProps) => {
+  return (
+    <Dialog.Root open={open} onOpenChange={onOpenChange}>
+      <Dialog.Portal>
+        <StyledDialogOverlay />
+        <StyledDialogContent>
+          <StyledContentInnerContainer>
+            <StyledPpussungIcon src={PpussungIcon} alt="뿌슝이" />
+            <StyledDialogTitle>'숨쉴때'에서 로그아웃할까요?</StyledDialogTitle>
+            <StyledDialogDescription>
+              다른 사용자의 접근을 막기 위해 어쩌구 저쩌구(생략가능)
+            </StyledDialogDescription>
+          </StyledContentInnerContainer>
+          <StyledButtonContainer>
+            <Dialog.Close asChild>
+              <BoxButton variant="filled" size="large" rounding={4}>
+                로그아웃
+              </BoxButton>
+            </Dialog.Close>
+            <Dialog.Close asChild>
+              <BoxButton variant="line" size="large" rounding={4}>
+                취소
+              </BoxButton>
+            </Dialog.Close>
+          </StyledButtonContainer>
+        </StyledDialogContent>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
