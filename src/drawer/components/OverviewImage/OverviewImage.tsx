185a93efc5bde296fd3e04a7b4d551c2b112042c
Chaeyeon Seo <94633589+seocylucky@users.noreply.github.com>
2024-03-02 00:54:11 +0900

    feat: 서랍장 등록 페이지 썸네일 이미지 및 소개 이미지 첨부 작업 (#78)
    * feat: 썸네일 이미지 첨부 작업
    
    * feat: 소개 이미지 첨부 작업
    
    * refactor: console.log 삭제
    
    * style: OverviewImage와 ThumbnailInput 간격 조정
    
    * style: 반응형 24.375rem -> 30rem으로 수정
    
    * feat: 첨부된 파일 삭제 버튼 추가
    
    + OverviewImage.style 레이아웃 스타일 관련 수정
    
    * refactor: console.log 삭제
    
    * style: 폰트 수정
    
    * style: margin-top 수정
    
    * rename: 철자 틀린 부분 수정
    
    + isWarned 옵셔널로 변경
    
    * refactor: OverviewFileUpload 따로 컴포넌트로 분리 및 key 관련 수정
    
    + splice -> filter 로 수정
    + handleFileChange 핸들러 수정
    
    * remove: console.log
    
    ---------
    
    Co-authored-by: Hanna922 <hanna.urssu@gmail.com>

---
+++ src/drawer/components/OverviewImage/OverviewImage.tsx
@@ -0,0 +1,63 @@
+import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
+
+import { OverviewFileUpload } from './OverviewFileUpload/OverviewFileUpload';
+import {
+  StyledImageUpload,
+  StyledOverviewContainer,
+  StyledOverviewDescription,
+  StyledOverviewTitle,
+  StyledOverviewTitleContainer,
+} from './OverviewImage.style';
+
+interface OverviewProps extends InputHTMLAttributes<HTMLLabelElement> {
+  isWarned?: boolean;
+}
+
+export const OverviewImage = ({ isWarned }: OverviewProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const MAX_FILE_COUNT = 5;
+
+  const handleFileChange = (file: File | undefined, index: number) => {
+    setFiles(
+      (prevFiles) => prevFiles.map((prevFile, i) => (i === index ? file : prevFile)) as File[]
+    );
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      handleFileChange(file, index);
+    }
+  };
+
+  const handleDeleteFile = (index: number) => {
+    setFiles((prevFiles) => {
+      const newFiles = prevFiles.filter((_, i) => i !== index);
+      return newFiles;
+    });
+  };
+
+  return (
+    <StyledOverviewContainer>
+      <StyledOverviewTitleContainer>
+        <StyledOverviewTitle $isWarned={isWarned}>소개 이미지 *</StyledOverviewTitle>
+        <StyledOverviewDescription>(권장 : 1920px X 1080px, 최대 5장)</StyledOverviewDescription>
+      </StyledOverviewTitleContainer>
+      <StyledImageUpload>
+        {Array.from({ length: MAX_FILE_COUNT }).map((_, index) => {
+          const file = files[index];
+          const key = file ? `file-${file.name}-${file.lastModified}` : `file-empty-${index}`;
+          return (
+            <OverviewFileUpload
+              key={key}
+              index={index}
+              file={file}
+              handleInputChange={handleInputChange}
+              handleDeleteFile={handleDeleteFile}
+            />
+          );
+        })}
+      </StyledImageUpload>
+    </StyledOverviewContainer>
+  );
+};
