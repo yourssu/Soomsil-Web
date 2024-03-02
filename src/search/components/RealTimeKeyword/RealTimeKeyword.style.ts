0139e3c45df6efcd51351ac1b77fcad22027a494
george <hyunrim03@naver.com>
2024-03-02 13:48:30 +0900

    fix: 최대 너비 스타일 수정

--- src/search/components/RealTimeKeyword/RealTimeKeyword.style.ts
+++ src/search/components/RealTimeKeyword/RealTimeKeyword.style.ts
@@ -4,6 +4,7 @@ export const StyledContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 1.25rem;
+  max-width: 25rem;
   min-width: 25rem;
   height: 38.7rem;
   gap: 0.625rem;
