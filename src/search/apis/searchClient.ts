c966a50f47ba3a9408ca25790c3726ba0fc917c3
Poly <95522176+indianaPoly@users.noreply.github.com>
2024-03-03 11:06:02 +0900

    feat: 실시간 검색어 API 연동 (#85)
    * design: 실시간 검색어 키워드 높이 추가
    
    * refactor: API CORS 에러 수정에 따라 BaseURL 변경
    
    * feat: 실시간 검색어 키워드 API
    
    * feat: 실시간 검색어 키워드 데이터 타입 설정
    
    * feat: 기존 시간 로직 변경
    
    * chore: 메인 화면 초기화
    
    * feat: 실시간 검색어 키워드 쿼리 설정
    
    * docs: 기존 search 삭제
    
    * fix: min-width 적용
    
    * fix: 최대 너비 스타일 수정
    
    * fix: Suspense 사용을 통한 로직 간단화
    
    * fix: useSuspenseQuery로 수정
    
    * refactor: query 배열에 대해서 readonly 타입 설정
    
    * feat: formatDateTime 함수 -> utils 분리
    
    * chore: webp 이미지 추가
    
    * design: webp 적용
    
    * chore: 이미지 삭제
    
    ---------
    
    Co-authored-by: george <hyunrim03@naver.com>

--- src/search/apis/searchClient.ts
+++ src/search/apis/searchClient.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export const searchClient = axios.create({
-  // CORS 오류 해결이 아직 안되어서 임의로 vite에서 proxy 설정하여 태스트중입니다.
-  // 추후 백엔드 문제 해결 후, 수정 예정
-  baseURL: '/api',
+  baseURL: 'http://52.78.169.59:8084',
 });
