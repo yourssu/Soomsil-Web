44079f7f1486ba09100e7b5c3374400628ddba4f
JjungminLee <julie0964@naver.com>
2024-03-04 15:03:10 +0900

    feat:Homelayout변경

--- src/home/components/HomeLayout/HomeLayout.tsx
+++ src/home/components/HomeLayout/HomeLayout.tsx
@@ -1,16 +1,29 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 
+import moment from 'moment';
 import { Outlet, useLocation } from 'react-router-dom';
+
+import api from '@/service/TokenService';
 
 import { Nav } from '../Nav/Nav';
 
 import { StyledLayout } from './HomeLayout.style';
 export const HomeLayout = ({ children }: PropsWithChildren) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const router = useLocation();
+  const accessToken = api.getAccessToken();
+  const accessExpiredIn = sessionStorage.getItem('accessExpiredIn');
+  useEffect(() => {
+    if (accessToken == null || moment(accessExpiredIn).diff(moment()) < 0) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  }, [accessToken, accessExpiredIn]);
 
   return (
     <StyledLayout>
-      {router.pathname === '/' ? <Nav isLoggedIn={false} /> : null}
+      {router.pathname === '/' ? <Nav isLoggedIn={isLoggedIn} /> : null}
       {children || <Outlet />}
     </StyledLayout>
   );
