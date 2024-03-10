dea558d2de20975790002324b8fe9f482779d622
2wndrhs <2wndrhs@gmail.com>
2024-03-10 17:46:27 +0900

    feat: `registerProduct()` API 요청 함수 추가

---
+++ src/drawer/apis/registerProduct.ts
@@ -0,0 +1,23 @@
+import { ImageResult } from '../types/ImageListResponse.type';
+
+import { drawerClient } from './drawerClient';
+
+const renameImageKeys = (image: ImageResult) => {
+  return {
+    key: image.imageKey,
+    url: image.imageUrl,
+  };
+};
+
+// TODO: 추후 product 타입 수정 필요
+export const registerProduct = async (product: unknown, images: ImageResult[]) => {
+  const [thumbnailImage, ...introductionImages] = images;
+
+  const response = await drawerClient.post('/v2/drawer', {
+    ...product,
+    thumbnailImage: renameImageKeys(thumbnailImage),
+    introductionImages: introductionImages.map(renameImageKeys),
+  });
+
+  return response.data;
+};
