4029f539d72ab557503382a30161037bfa5ec7dc
2wndrhs <2wndrhs@gmail.com>
2024-03-10 20:37:04 +0900

    feat: `useRegisterProduct()` 커스텀 훅 추가

---
+++ src/drawer/hooks/useRegisterProduct.ts
@@ -0,0 +1,16 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { registerProduct } from '@/drawer/apis/registerProduct';
+import { uploadImages } from '@/drawer/apis/uploadImages';
+
+export const useRegisterProduct = () => {
+  return useMutation({
+    mutationFn: async (data: unknown) => {
+      const imageList = [...data.thumbnailImage, ...data.introductionImages] as File[];
+      const images = await uploadImages(imageList);
+
+      const response = await registerProduct(data, images);
+      return response;
+    },
+  });
+};
