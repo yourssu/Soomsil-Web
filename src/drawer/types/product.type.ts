a323355e53343cd9fa611c69b92e9600738494c8
nijuy <youjin6325@naver.com>
2024-03-10 21:03:42 +0900

    feat: `ProductResult` `ProductResponses` 타입 생성

--- src/drawer/types/product.type.ts
+++ src/drawer/types/product.type.ts
@@ -20,6 +20,26 @@ export interface ProductDetailResult {
   webpageUrl?: string;
 }
 
+export interface ProductResult {
+  actionContent?: string;
+  actionType: 'BROWSER' | 'DEEPLINK' | 'WEB_VIEW';
+  category: 'CAMPUS' | 'ETC' | 'HEALTH' | 'HOBBY' | 'IT' | 'KNOWLEDGE' | 'LIFESTYLE';
+  count: number;
+  expireDay: number;
+  introductionImage: string[];
+  isAd: boolean;
+  mainImage: string;
+  productBookmarkKey?: number;
+  productNo: number;
+  productSubTitle: string;
+  productTitle: string;
+  status: 'ACTIVE' | 'INACTIVE';
+}
+
 export interface ProductDetailResponse {
   detail: ProductDetailResult;
+}
+
+export interface ProductResponses {
+  productList: ProductResult[];
 }
