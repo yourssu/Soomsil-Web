ae291ef948b5b82e4627dd2fdcaee3486da53431
seocylucky <seocylucky@github.com>
2024-03-12 05:00:18 +0900

    feat: StarRanking 페이지 작업

---
+++ src/drawer/pages/StarRanking/StarRanking.tsx
@@ -0,0 +1,51 @@
+import { Category } from '@/drawer/components/Category/Category';
+import { BigDrawerCard } from '@/drawer/components/DrawerCard/BigDrawerCard';
+import { useGetStarRank } from '@/drawer/hooks/useGetStarRank';
+
+import {
+  StyledBetweenContainer,
+  StyledCardContainer,
+  StyledContainer,
+  StyledDescription,
+  StyledRankingContainer,
+  StyledTextContainer,
+  StyledTitle,
+} from '../Ranking/Ranking.style';
+
+export const StarRanking = () => {
+  const { rankings, setSelectedCategory } = useGetStarRank();
+
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <StyledContainer>
+      <Category isAll={true} isRank={true} onCategorySelect={handleCategorySelect} />
+      <StyledRankingContainer>
+        <div>
+          <StyledBetweenContainer>
+            <StyledTextContainer>
+              <StyledTitle>Star Ranking</StyledTitle>
+              <StyledDescription>실시간 랭킹을 확인해보세요.</StyledDescription>
+            </StyledTextContainer>
+          </StyledBetweenContainer>
+          <StyledCardContainer>
+            {rankings.map((product) => (
+              <BigDrawerCard
+                key={product.productNo}
+                link={''}
+                title={product.productTitle}
+                body={product.productSubTitle}
+                bookmarkCount={product.productBookmarkKey}
+                isBookmarked={product.isBookmarked}
+                bigImgSrc={product.mainImage}
+                smallImgSrc={product.introductionImage[0]}
+              />
+            ))}
+          </StyledCardContainer>
+        </div>
+      </StyledRankingContainer>
+    </StyledContainer>
+  );
+};
