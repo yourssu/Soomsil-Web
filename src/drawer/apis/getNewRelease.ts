b963fa8af4f8e77b883f97935295f3d7317e9d7e
seocylucky <seocylucky@github.com>
2024-03-18 18:58:52 +0900

    feat: 불필요한 then 관련 코드 삭제

--- src/drawer/apis/getNewRelease.ts
+++ src/drawer/apis/getNewRelease.ts
@@ -4,15 +4,11 @@ import { ProductListResponse } from '../types/ProductList.type';
 import { drawerClient } from './drawerClient';
 
 export const getNewRelease = async ({ responseType, category }: GetRanking) => {
-  const response = await drawerClient
-    .get<ProductListResponse>('/v2/drawer/new-release', {
-      params: {
-        responseType,
-        category,
-      },
-    })
-    .then((response) => {
-      return response;
-    });
+  const response = await drawerClient.get<ProductListResponse>('/v2/drawer/new-release', {
+    params: {
+      responseType,
+      category,
+    },
+  });
   return response.data;
 };
--- src/drawer/apis/getRanking.ts
+++ src/drawer/apis/getRanking.ts
@@ -4,15 +4,11 @@ import { ProductListResponse } from '../types/ProductList.type';
 import { drawerClient } from './drawerClient';
 
 export const getRanking = async ({ responseType, category }: GetRanking) => {
-  const response = await drawerClient
-    .get<ProductListResponse>('/v2/drawer/rank', {
-      params: {
-        responseType,
-        category,
-      },
-    })
-    .then((response) => {
-      return response;
-    });
+  const response = await drawerClient.get<ProductListResponse>('/v2/drawer/rank', {
+    params: {
+      responseType,
+      category,
+    },
+  });
   return response.data;
 };
