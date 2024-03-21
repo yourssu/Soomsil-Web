2f8ed1e0d91cfc6d3e1f0d76a85c5fbccda52f3e
JjungminLee <julie0964@naver.com>
2024-03-21 23:05:00 +0900

    refactor: export const로 수정

--- src/home/apis/customedAxios.ts
+++ src/home/apis/customedAxios.ts
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-const customedAxios = axios.create({
+export const customedAxios = axios.create({
   baseURL: import.meta.env.VITE_API_HOME_URL,
   headers: {
     'Content-Type': 'application/json',
     withCredentials: true,
   },
 });
-
-export default customedAxios;
