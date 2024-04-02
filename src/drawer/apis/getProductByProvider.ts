caf0697175b3b4bdcf951b53c5c2f1792d1ffedf
nijuy <youjin6325@naver.com>
2024-04-02 20:05:09 +0900

    feat: 개발자 서비스 관련 api, hook 수정
    - page 기본 값을 0으로 수정, category 추가

--- src/drawer/apis/getProductByProvider.ts
+++ src/drawer/apis/getProductByProvider.ts
@@ -4,16 +4,19 @@ import { drawerClient } from './drawerClient';
 
 export const getProductByProvider = async ({
   providerId,
-  page = 1,
+  page = 0,
+  category = '',
 }: {
   providerId: string;
   page?: number;
+  category?: string;
 }): Promise<ProductResponses> => {
   const { data } = await drawerClient.get('/v2/drawer', {
     params: {
       responseType: 'WEB',
       providerId,
       page,
+      category,
     },
   });
   return data;
--- src/drawer/hooks/useGetProductByProvider.ts
+++ src/drawer/hooks/useGetProductByProvider.ts
@@ -4,15 +4,17 @@ import { getProductByProvider } from '../apis/getProductByProvider';
 
 export const useGetProductByProvider = ({
   providerId,
-  page = 1,
+  page = 0,
+  category = '',
 }: {
   providerId: string;
   page?: number;
+  category?: string;
 }) => {
   return useQuery({
-    queryKey: ['productByProvider', providerId],
+    queryKey: ['productByProvider', providerId, category],
     queryFn: () => {
-      return getProductByProvider({ providerId, page });
+      return getProductByProvider({ providerId, page, category });
     },
     select: (data) => data.productList,
   });
