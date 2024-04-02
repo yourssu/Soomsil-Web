116feead76eb6ac7207949cca168376bdefff704
nijuy <youjin6325@naver.com>
2024-04-02 20:18:45 +0900

    feat: `Provider` 페이지 내 더미 데이터 삭제

--- src/drawer/pages/Provider/Provider.tsx
+++ src/drawer/pages/Provider/Provider.tsx
@@ -1,7 +1,12 @@
 import { useParams } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
 import { CardLayout } from '@/drawer/components/CardLayout/CardLayout';
 import { Category } from '@/drawer/components/Category/Category';
+import { EmptyScreen } from '@/drawer/components/EmptyScreen/EmptyScreen';
+import { NOT_FOUND_TEXT } from '@/drawer/constants/empty.constant';
+import { useGetProductByProvider } from '@/drawer/hooks/useGetProductByProvider';
+import { CategoryState } from '@/drawer/recoil/CategoryState';
 
 import {
   StyledDescription,
@@ -10,20 +15,28 @@ import {
   StyledProviderName,
 } from './Provider.style';
 
-const Dummy = Array.from({ length: 12 }).map((_, index) => index);
-
 export const Provider = () => {
   const { providerId } = useParams();
+  const [selectedCategory, setSelectedCategory] = useRecoilState(CategoryState);
+
+  const { data } = useGetProductByProvider({
+    providerId: String(providerId),
+    category: selectedCategory,
+  });
 
   return (
     <StyledContainer>
-      <Category isAll={true} />
+      <Category isAll={true} handleCategorySelect={setSelectedCategory} isRank={true} />
       <StyledProviderContainer>
         <div>
           <StyledProviderName>{providerId}</StyledProviderName>
           <StyledDescription>개발자의 서비스를 확인해보세요.</StyledDescription>
         </div>
-        <CardLayout data={Dummy} type={'PROVIDER'} />
+        {data?.length ? (
+          <CardLayout data={data} type={'PROVIDER'} />
+        ) : (
+          <EmptyScreen {...NOT_FOUND_TEXT['PROVIDER']} type={'PROVIDER'} />
+        )}
       </StyledProviderContainer>
     </StyledContainer>
   );
