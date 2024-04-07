7274bdc7ba68ca036ce9b198a94bdbdab60b5e0d
seocylucky <seocylucky@github.com>
2024-04-07 15:06:44 +0900

    feat: 내 서랍장 페이지 api 연결 작업

--- src/drawer/pages/MyDrawer/MyDrawer.tsx
+++ src/drawer/pages/MyDrawer/MyDrawer.tsx
@@ -8,20 +8,24 @@ import { CardLayout } from '@/drawer/components/CardLayout/CardLayout';
 import { EmptyScreen } from '@/drawer/components/EmptyScreen/EmptyScreen';
 import { NOT_FOUND_TEXT } from '@/drawer/constants/empty.constant';
 import { TAB_DESCRIPTION } from '@/drawer/constants/mydrawer.constant';
+import { useGetBookmarked } from '@/drawer/hooks/useGetBookMarked';
+import { useGetMyRegistered } from '@/drawer/hooks/useGetMyRegistered';
 
 import { StyledContainer, StyledTabWrapper, StyledDescription } from './MyDrawer.style';
-
-const Dummy = Array.from({ length: 12 }).map((_, index) => index);
 
 export type TabType = 'STAR' | 'MYDRAWER';
 
 export const MyDrawer = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { data: bookmarkedData } = useGetBookmarked({ responseType: 'WEB', page: 0 });
+  const { data: myProductData } = useGetMyRegistered({ responseType: 'WEB', page: 0 });
   const initialTab = searchParams.get('tab');
 
   const [currentTab, setCurrentTab] = useState<TabType>(
     isTabType(initialTab) ? initialTab : 'STAR'
   );
+
+  const drawerData = currentTab === 'STAR' ? bookmarkedData : myProductData;
 
   function isTabType(tab: string | null): tab is TabType {
     return tab === 'STAR' || tab === 'MYDRAWER';
@@ -52,10 +56,10 @@ export const MyDrawer = () => {
         />
       </StyledTabWrapper>
       <Spacing direction={'vertical'} size={14} />
-      {Dummy ? (
+      {drawerData?.length ? (
         <>
           <StyledDescription>{TAB_DESCRIPTION[currentTab]}</StyledDescription>
-          <CardLayout data={Dummy} type={currentTab} />
+          <CardLayout data={drawerData} type={currentTab} />
         </>
       ) : (
         <EmptyScreen {...NOT_FOUND_TEXT[currentTab]} type={currentTab} />
