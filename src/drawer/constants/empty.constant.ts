6fa215421d265cf5db5ddfe43bb0bff0e943d6d5
이유진 <youjin6325@naver.com>
2024-04-11 17:10:27 +0900

    feat: 개발자의 서비스 더보기 페이지 (#123)
    * feat: 제작자 서비스 페이지 퍼블리싱
    
    * feat: url에서 provierId 따오기
    
    * fix: `ProductResult` 타입에 `isBookmarked` 속성 추가
    
    * design: `CardSmallThumbnail` 컴포넌트에 $height 추가
    
    * design: `StyledProviderContainer`의 gap삭제
    
    - `CardLayout`에 margin-top: 2rem;과 중복됨
    
    * feat: 개발자 서비스 관련 api, hook 수정
    
    - page 기본 값을 0으로 수정, category 추가
    
    * refactor: `CardLayout`에 있던 임시 타입, props 수정
    
    * feat: 개발자 서비스 더보기 페이지에서 사용할 EmptyScreen 문구 추가
    
    * feat: `Provider` 페이지 내 더미 데이터 삭제
    
    * design: Soomsil/title22 -> YDS/title3
    
    Co-authored-by: 이중곤 <2wndrhs@gmail.com>
    
    * refactor: api 함수 인자를 인터페이스로 분리
    
    * refactor: data.length > 0 조건 명시하기
    
    - data 조건은 useSuspenseQuery로 바꾸면서 삭제 가능할듯
    
    * refactor: 화면 작아졌을 때 카테고리를 드롭다운 형식으로 변경
    
    ---------
    
    Co-authored-by: 이중곤 <2wndrhs@gmail.com>

--- src/drawer/constants/empty.constant.ts
+++ src/drawer/constants/empty.constant.ts
@@ -1,5 +1,5 @@
 export const NOT_FOUND_TEXT: Record<
-  'SEARCH' | 'STAR' | 'MYDRAWER',
+  'SEARCH' | 'STAR' | 'MYDRAWER' | 'PROVIDER',
   { boldText: string; subText: string }
 > = {
   SEARCH: {
@@ -13,5 +13,9 @@ export const NOT_FOUND_TEXT: Record<
   MYDRAWER: {
     boldText: '내가 등록한 서비스가 없습니다.',
     subText: '새로운 서비스를 등록해보세요.',
+  },
+  PROVIDER: {
+    boldText: '아직 해당 카테고리에 업로드한 서비스가 없습니다.',
+    subText: '',
   },
 };
--- src/drawer/hooks/useGetProductByProvider.ts
+++ src/drawer/hooks/useGetProductByProvider.ts
@@ -1,18 +1,23 @@
 import { useQuery } from '@tanstack/react-query';
+import { useRecoilValue } from 'recoil';
+
+import { CategoryState } from '@/drawer/recoil/CategoryState';
 
 import { getProductByProvider } from '../apis/getProductByProvider';
 
 export const useGetProductByProvider = ({
   providerId,
-  page = 1,
+  page = 0,
 }: {
   providerId: string;
   page?: number;
 }) => {
+  const selectedCategory = useRecoilValue(CategoryState);
+
   return useQuery({
-    queryKey: ['productByProvider', providerId],
+    queryKey: ['productByProvider', providerId, selectedCategory],
     queryFn: () => {
-      return getProductByProvider({ providerId, page });
+      return getProductByProvider({ providerId, page, category: selectedCategory });
     },
     select: (data) => data.productList,
   });
