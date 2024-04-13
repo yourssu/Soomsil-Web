5b242cf7a55f187ef671c2f0a4a0a75a6404efc4
aaminha <dks4857@gmail.com>
2024-04-13 16:06:11 +0900

    style: 불필요한 async 삭제

--- src/home/hooks/useGetDrawerRanking.ts
+++ src/home/hooks/useGetDrawerRanking.ts
@@ -5,8 +5,8 @@ import { getRanking } from '@/drawer/apis/getRanking';
 export const useGetDrawerRanking = () => {
   return useQuery({
     queryKey: ['drawerRanking'],
-    queryFn: async () => {
-      return await getRanking({ responseType: 'WEB', category: '', page: 0 });
+    queryFn: () => {
+      return getRanking({ responseType: 'WEB', category: '', page: 0 });
     },
   });
 };
