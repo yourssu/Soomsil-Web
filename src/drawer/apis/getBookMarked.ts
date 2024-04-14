dafa569436253f0614f13da5ea17d0d83551cb25
fecapark <fecapark@gmail.com>
2024-04-15 00:23:27 +0900

    fix: drawerClient -> soomsilClient 로 변경

--- src/drawer/apis/getBookMarked.ts
+++ src/drawer/apis/getBookMarked.ts
@@ -1,10 +1,10 @@
+import { soomsilClient } from '@/apis';
+
 import { RankingRequestParams } from '../types/RankingRequestParams.type';
 import { ProductResponses } from '../types/product.type';
 
-import { drawerClient } from './drawerClient';
-
 export const getBookmarked = async ({ responseType, page }: RankingRequestParams) => {
-  const response = await drawerClient.get<ProductResponses>('/v2/drawer/my-bookmarked', {
+  const response = await soomsilClient.get<ProductResponses>('/v2/drawer/my-bookmarked', {
     params: {
       responseType,
       page,
