bd741f550c703e1abba967fe8c7f654a4f0b4505
nijuy <youjin6325@naver.com>
2024-04-16 00:19:55 +0900

    refactor: `useGetProductDetail`에 useSuspenseQuery 사용
    - data !== undefined인 점이 보장되므로 `ServiceDetail`에서 isSuccess 관련 코드 삭제

--- src/drawer/pages/ServiceDetail/ServiceDetail.tsx
+++ src/drawer/pages/ServiceDetail/ServiceDetail.tsx
@@ -50,7 +50,7 @@ const Category = {
 
 export const ServiceDetail = () => {
   const { serviceId } = useParams();
-  const { isSuccess, data } = useGetProductDetail(Number(serviceId));
+  const { data } = useGetProductDetail(Number(serviceId));
 
   const theme = useTheme();
 
@@ -69,127 +69,121 @@ export const ServiceDetail = () => {
   };
 
   return (
-    <>
-      {isSuccess && (
-        <StyledServiceDetailContainer>
-          <StyledBackgroundImageContainer $backgroundImage={data.thumbnail}>
-            <StyledServiceTitleText>{data.productTitle}</StyledServiceTitleText>
-            <StyledServiceDeveloperText>{data.providerId}</StyledServiceDeveloperText>
-            <StyledServiceInfoContainer>
-              <StyledThumbnailImage src={data.thumbnail} />
-              <StyledCategoryContainer>
-                <StyledCategoryText>{Category[data.category]}</StyledCategoryText>
-                <StyledCategoryHintText>카테고리</StyledCategoryHintText>
-              </StyledCategoryContainer>
-            </StyledServiceInfoContainer>
-            <StyledServiceActionContainer>
-              {/* TODO: 버튼 사이즈 반응형 적용 필요 */}
-              <BoxButton
-                size="medium"
-                rounding={8}
-                variant="filled"
-                width="200px"
-                onClick={() => {
-                  const { appStoreUrl, googlePlayUrl, webpageUrl } = data;
-                  window.open(appStoreUrl || googlePlayUrl || webpageUrl);
+    <StyledServiceDetailContainer>
+      <StyledBackgroundImageContainer $backgroundImage={data.thumbnail}>
+        <StyledServiceTitleText>{data.productTitle}</StyledServiceTitleText>
+        <StyledServiceDeveloperText>{data.providerId}</StyledServiceDeveloperText>
+        <StyledServiceInfoContainer>
+          <StyledThumbnailImage src={data.thumbnail} />
+          <StyledCategoryContainer>
+            <StyledCategoryText>{Category[data.category]}</StyledCategoryText>
+            <StyledCategoryHintText>카테고리</StyledCategoryHintText>
+          </StyledCategoryContainer>
+        </StyledServiceInfoContainer>
+        <StyledServiceActionContainer>
+          {/* TODO: 버튼 사이즈 반응형 적용 필요 */}
+          <BoxButton
+            size="medium"
+            rounding={8}
+            variant="filled"
+            width="200px"
+            onClick={() => {
+              const { appStoreUrl, googlePlayUrl, webpageUrl } = data;
+              window.open(appStoreUrl || googlePlayUrl || webpageUrl);
+            }}
+          >
+            DOWNLOAD
+          </BoxButton>
+          {data.githubUrl && (
+            /* TODO: 버튼 사이즈 반응형 적용 필요*/
+            <BoxButton
+              size="medium"
+              rounding={8}
+              variant="tinted"
+              width="200px"
+              onClick={() => {
+                window.open(data.githubUrl);
+              }}
+            >
+              Github
+            </BoxButton>
+          )}
+          <StyledIconButtonContainer>
+            <button>
+              <IconContext.Provider
+                value={{
+                  color: theme.color.pointViolet,
+                  size: '1.5rem',
                 }}
               >
-                DOWNLOAD
-              </BoxButton>
-              {data.githubUrl && (
-                /* TODO: 버튼 사이즈 반응형 적용 필요*/
-                <BoxButton
-                  size="medium"
-                  rounding={8}
-                  variant="tinted"
-                  width="200px"
-                  onClick={() => {
-                    window.open(data.githubUrl);
-                  }}
-                >
-                  Github
-                </BoxButton>
-              )}
-              <StyledIconButtonContainer>
-                <button>
-                  <IconContext.Provider
-                    value={{
-                      color: theme.color.pointViolet,
-                      size: '1.5rem',
-                    }}
-                  >
-                    <IcShareLine />
-                  </IconContext.Provider>
-                </button>
-                <StyledIconLabelText $color={theme.color.pointViolet}>SHARE</StyledIconLabelText>
-              </StyledIconButtonContainer>
-              <StyledIconButtonContainer>
-                <button>
-                  <IconContext.Provider
-                    value={{
-                      color: theme.color.pointYellow,
-                      size: '1.5rem',
-                    }}
-                  >
-                    {data.isBookmarked ? <IcStarFilled /> : <IcStarLine />}
-                  </IconContext.Provider>
-                </button>
-                <StyledIconLabelText $color={theme.color.pointYellow}>
-                  Recommend
-                </StyledIconLabelText>
-              </StyledIconButtonContainer>
-            </StyledServiceActionContainer>
-          </StyledBackgroundImageContainer>
-          <StyledLowerSection>
-            <StyledDescriptionSection>
-              <StyledCarousel ref={scrollRef}>
-                {data.introductionImage.length > 1 && (
-                  <>
-                    <StyledCarouselButton
-                      $backgroundImage={carouselLeftButton}
-                      $left={'-24px'}
-                      type="button"
-                      name="left"
-                      onClick={handleCarouselClick}
-                    />
-                    <StyledCarouselButton
-                      $backgroundImage={carouselRightButton}
-                      $right={'-24px'}
-                      type="button"
-                      name="right"
-                      onClick={handleCarouselClick}
-                    />
-                  </>
-                )}
-                {data.introductionImage.map((imageSrc) => (
-                  <StyledProductImage src={imageSrc} key={imageSrc} />
-                ))}
-              </StyledCarousel>
-              <StyledDescriptionPart>
-                <StyledSubtitle>{`추천`}</StyledSubtitle>
-                <StyledDescription>
-                  {data.count}+
-                  <IconContext.Provider value={{ size: '15px', color: '#FDD655' }}>
-                    <IcStarFilled />
-                  </IconContext.Provider>
-                </StyledDescription>
-              </StyledDescriptionPart>
+                <IcShareLine />
+              </IconContext.Provider>
+            </button>
+            <StyledIconLabelText $color={theme.color.pointViolet}>SHARE</StyledIconLabelText>
+          </StyledIconButtonContainer>
+          <StyledIconButtonContainer>
+            <button>
+              <IconContext.Provider
+                value={{
+                  color: theme.color.pointYellow,
+                  size: '1.5rem',
+                }}
+              >
+                {data.isBookmarked ? <IcStarFilled /> : <IcStarLine />}
+              </IconContext.Provider>
+            </button>
+            <StyledIconLabelText $color={theme.color.pointYellow}>Recommend</StyledIconLabelText>
+          </StyledIconButtonContainer>
+        </StyledServiceActionContainer>
+      </StyledBackgroundImageContainer>
+      <StyledLowerSection>
+        <StyledDescriptionSection>
+          <StyledCarousel ref={scrollRef}>
+            {data.introductionImage.length > 1 && (
+              <>
+                <StyledCarouselButton
+                  $backgroundImage={carouselLeftButton}
+                  $left={'-24px'}
+                  type="button"
+                  name="left"
+                  onClick={handleCarouselClick}
+                />
+                <StyledCarouselButton
+                  $backgroundImage={carouselRightButton}
+                  $right={'-24px'}
+                  type="button"
+                  name="right"
+                  onClick={handleCarouselClick}
+                />
+              </>
+            )}
+            {data.introductionImage.map((imageSrc) => (
+              <StyledProductImage src={imageSrc} key={imageSrc} />
+            ))}
+          </StyledCarousel>
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`추천`}</StyledSubtitle>
+            <StyledDescription>
+              {data.count}+
+              <IconContext.Provider value={{ size: '15px', color: '#FDD655' }}>
+                <IcStarFilled />
+              </IconContext.Provider>
+            </StyledDescription>
+          </StyledDescriptionPart>
 
-              <StyledDescriptionPart>
-                <StyledSubtitle>{`서비스 소개`}</StyledSubtitle>
-                <StyledDescription>{data.productContent}</StyledDescription>
-              </StyledDescriptionPart>
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`서비스 소개`}</StyledSubtitle>
+            <StyledDescription>{data.productContent}</StyledDescription>
+          </StyledDescriptionPart>
 
-              <StyledDescriptionPart>
-                <StyledSubtitle>{`저작권`}</StyledSubtitle>
-                <StyledDescription>{data.providerId}</StyledDescription>
-              </StyledDescriptionPart>
-            </StyledDescriptionSection>
+          <StyledDescriptionPart>
+            <StyledSubtitle>{`저작권`}</StyledSubtitle>
+            <StyledDescription>{data.providerId}</StyledDescription>
+          </StyledDescriptionPart>
+        </StyledDescriptionSection>
 
-            <MoreProductSection providerId={data.providerId} />
-          </StyledLowerSection>
-        </StyledServiceDetailContainer>
-      )}
-    </>
+        <MoreProductSection providerId={data.providerId} />
+      </StyledLowerSection>
+    </StyledServiceDetailContainer>
   );
 };
