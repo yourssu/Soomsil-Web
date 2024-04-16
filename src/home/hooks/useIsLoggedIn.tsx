90aaddeb990be8ff09d8790cc0442fce04192349
aaminha <dks4857@gmail.com>
2024-04-17 01:54:47 +0900

    style: Home에서 로그인 여부 확인 로직을 useIsLoggedIn 커스텀 훅으로 분리

---
+++ src/home/hooks/useIsLoggedIn.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { isPast } from 'date-fns';
+import { useRecoilValue } from 'recoil';
+
+import { useGetUserData } from '@/home/hooks/useGetUserData';
+import { UserState } from '@/home/recoil/UserState';
+import { api } from '@/service/TokenService';
+export const useIsLoggedIn = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const accessToken = api.getAccessToken();
+  const accessTokenExpiredIn = sessionStorage.getItem('accessExpiredIn');
+  const currentUser = useRecoilValue(UserState);
+  const { refetch } = useGetUserData();
+
+  const isAccessTokenExpired = () => {
+    if (!accessTokenExpiredIn) return false;
+    if (!accessToken) return false;
+    return isPast(new Date(accessTokenExpiredIn));
+  };
+
+  useEffect(() => {
+    if (!accessTokenExpiredIn) return;
+
+    if (isAccessTokenExpired()) {
+      setIsLoggedIn(false);
+      return;
+    }
+
+    setIsLoggedIn(true);
+
+    if (!currentUser) refetch();
+  }, [accessToken, accessTokenExpiredIn]);
+
+  return isLoggedIn;
+};
--- src/home/pages/Home/Home.tsx
+++ src/home/pages/Home/Home.tsx
@@ -1,17 +1,10 @@
-import { useEffect, useState } from 'react';
-
-import { isPast } from 'date-fns';
-import { useRecoilValue } from 'recoil';
-
 import { DrawerRanking } from '@/home/components/DrawerRanking/DrawerRanking';
 import { Header } from '@/home/components/Header/Header';
 import { Nav } from '@/home/components/Nav/Nav';
 import { Notification } from '@/home/components/Notification/Notification';
 import { SearchKeyword } from '@/home/components/SearchKeyword/SearchKeyword';
 import { SocialNetworkService } from '@/home/components/SocialNetworkService/SocialNetworkService';
-import { useGetUserData } from '@/home/hooks/useGetUserData';
-import { UserState } from '@/home/recoil/UserState';
-import { api } from '@/service/TokenService';
+import { useIsLoggedIn } from '@/home/hooks/useIsLoggedIn';
 
 import {
   StyledComponentContainer,
@@ -20,22 +13,7 @@ import {
 } from './Home.style';
 
 export const Home = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const accessToken = api.getAccessToken();
-  const accessExpiredIn = sessionStorage.getItem('accessExpiredIn');
-  const currentUser = useRecoilValue(UserState);
-  const { refetch } = useGetUserData();
-
-  useEffect(() => {
-    if (accessExpiredIn) {
-      if (accessToken == null || isPast(new Date(accessExpiredIn))) {
-        setIsLoggedIn(false);
-      } else {
-        setIsLoggedIn(true);
-        if (!currentUser) refetch();
-      }
-    }
-  }, [accessToken, accessExpiredIn]);
+  const isLoggedIn = useIsLoggedIn();
 
   return (
     <StyledContainer>
