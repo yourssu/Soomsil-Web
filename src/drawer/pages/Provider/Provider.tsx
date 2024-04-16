771541a910d9e55461474db2e1ab79c39aa4f17b
nijuy <youjin6325@naver.com>
2024-04-17 01:21:58 +0900

    refactor: NOT_FOUND_TEXT를 props에서 `EmptyScreen` 내부로 이동

--- src/drawer/pages/Provider/Provider.tsx
+++ src/drawer/pages/Provider/Provider.tsx
@@ -4,7 +4,6 @@ import { CardLayout } from '@/drawer/components/CardLayout/CardLayout';
 import { CategoryDropdownMenu } from '@/drawer/components/Category/CategoryDropdownMenu/CategoryDropdownMenu';
 import { RankingCategory } from '@/drawer/components/Category/RankingCategory';
 import { EmptyScreen } from '@/drawer/components/EmptyScreen/EmptyScreen';
-import { NOT_FOUND_TEXT } from '@/drawer/constants/empty.constant';
 import { SMALL_DESKTOP_MEDIA_QUERY } from '@/drawer/constants/mobileview.constant';
 import { useGetProductByProvider } from '@/drawer/hooks/useGetProductByProvider';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
@@ -37,7 +36,7 @@ export const Provider = () => {
         {data && data.length > 0 ? (
           <CardLayout data={data} type={'PROVIDER'} />
         ) : (
-          <EmptyScreen {...NOT_FOUND_TEXT['PROVIDER']} type={'PROVIDER'} />
+          <EmptyScreen type={'PROVIDER'} />
         )}
       </StyledProviderContainer>
     </StyledContainer>
