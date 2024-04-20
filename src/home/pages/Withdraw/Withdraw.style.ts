da7b4f122957a702766b88136db9d59e8306b374
solmee <kii5541@naver.com>
2024-04-21 01:00:24 +0900

    feat: 계정탈퇴 페이지 퍼블리싱 구현 (#143)

--- src/home/pages/Withdraw/Withdraw.style.ts
+++ src/home/pages/Withdraw/Withdraw.style.ts
@@ -1,1 +1,73 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
+
+const Center = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const fontSize = (size: number, base: number = 16) => `
+  font-size: ${size}px;
+  font-size: calc(${size / base} * 1rem);
+`;
+
+export const StyledWrapper = styled.div`
+  ${Center}
+  gap: 24px;
+`;
+
+export const StyledWithdrawContainer = styled.div`
+  ${Center}
+
+  padding: 22px 24px 22px 24px;
+  gap: 24px;
+  width: 480px;
+  border-radius: 16px;
+  border: 1px solid ${({ theme }) => theme.color.borderNormal};
+`;
+
+export const TitleText = styled.p`
+  ${({ theme }) => theme.typo.title2};
+  ${fontSize(24)};
+  font-weight: 700;
+  line-height: 31.2px;
+  color: ${({ theme }) => theme.color.textSecondary};
+`;
+
+export const SubTitleTextThree = styled.p`
+  ${({ theme }) => theme.typo.subtitle3};
+  color: ${({ theme }) => theme.color.buttonNormal};
+  ${fontSize(20)};
+  font-weight: 500;
+  line-height: 26px;
+  align-self: self-start;
+`;
+
+export const ButtonText = styled.p`
+  ${fontSize(14)};
+  color: ${({ theme }) => theme.color.buttonDisabled};
+  font-weight: 400;
+  line-height: 18.2px;
+  text-align: left;
+  ${({ theme }) => theme.typo.button3};
+`;
+
+export const Left = styled.div`
+  align-self: self-start;
+`;
+
+export const SubTitleTextFive = styled.ul`
+  ${({ theme }) => theme.typo.subtitle5};
+  color: ${({ theme }) => theme.color.buttonNormal};
+  ${fontSize(16)};
+  font-weight: 500;
+  line-height: 20.8px;
+  text-align: left;
+  list-style-type: disc;
+  padding: 0px 24px;
+`;
+
+export const ListItem = styled.li`
+  margin-bottom: 10px;
+`;
--- src/home/pages/Withdraw/Withdraw.tsx
+++ src/home/pages/Withdraw/Withdraw.tsx
@@ -1,5 +1,67 @@
+import Logo from '@/assets/soomsil_v2_logo.svg';
+import {
+  StyledWrapper,
+  StyledWithdrawContainer,
+  TitleText,
+  SubTitleTextThree,
+  SubTitleTextFive,
+  ButtonText,
+  Left,
+  ListItem,
+} from './Withdraw.style';
+import { BoxButton, CheckBox } from '@yourssu/design-system-react';
+import { useState } from 'react';
+import { Withdrawn } from '@/home/components/WithdrawContents/Withdrawn/Withdrawn';
+
 export const Withdraw = () => {
-    return (
-        <></>
-    );
+  const [agreed, setAgreed] = useState(false);
+  const [draw, setDraw] = useState(false);
+
+  const checkAgree = () => {
+    setAgreed(!agreed);
+  };
+
+  const withdrawAgree = () => {
+    setDraw(true);
+  };
+
+  return (
+    <StyledWrapper>
+      <img src={Logo} alt={'Logo-image'} width={180} height={38} />
+      <StyledWithdrawContainer>
+        {draw ? (
+          <Withdrawn />
+        ) : (
+          <>
+            <TitleText>계정 탈퇴</TitleText>
+            <SubTitleTextThree>계정 탈퇴 전 꼭 확인하세요.</SubTitleTextThree>
+            <SubTitleTextFive>
+              <ListItem>탈퇴하는 즉시 데이터가 제거되며, 복구가 불가능합니다.</ListItem>
+              <ListItem>
+                닉네임, 이메일 등 사용자를 특정할 수 있는 모든 계정 정보가 지워집니다.
+              </ListItem>
+              <ListItem>
+                등록된 글이나 댓글의 삭제를 원한다면 탈퇴 이전에 삭제하시기 바랍니다.
+              </ListItem>
+            </SubTitleTextFive>
+            <Left>
+              <CheckBox size="medium" isSelected={agreed} onClick={checkAgree} type="button">
+                <ButtonText>안내사항을 확인하였으며, 이에 동의합니다.</ButtonText>
+              </CheckBox>
+            </Left>
+            <BoxButton
+              style={{ width: '100%' }}
+              rounding={8}
+              size="large"
+              variant="filled"
+              onClick={withdrawAgree}
+              disabled={!agreed}
+            >
+              탈퇴하기
+            </BoxButton>
+          </>
+        )}
+      </StyledWithdrawContainer>
+    </StyledWrapper>
+  );
 };
