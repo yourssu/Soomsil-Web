7c8befed076a8d4892c692fa0b452d20803fd320
jonique98 <tnals9899@naver.com>
2024-04-24 22:22:35 +0900

    feat: 마이페이지 하위 웹 정보 요소 클릭 시 링크 연결

--- src/home/components/MyMenuList/MyMenuList.tsx
+++ src/home/components/MyMenuList/MyMenuList.tsx
@@ -20,6 +20,10 @@ export const MyMenuList = () => {
   const theme = useTheme();
   const navigate = useNavigate();
 
+  const serviceTermsLink = 'https://yourssu.notion.site/fb10f214be754b1487b3cde19757cba9';
+  const privacyPolicyLink = 'https://yourssu.notion.site/b6249cbb5d614ba0be70b7cf4dcdf9cb';
+  // const openSourceLink = ''
+
   return (
     <>
       <StyledContainer>
@@ -47,13 +51,22 @@ export const MyMenuList = () => {
           </StyledMenuContainer>
           <StyledMenuContainer>
             <StyledMenuTitle>웹 정보</StyledMenuTitle>
-            <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            <StyledListItem
+              rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}
+              onClick={() => window.open(serviceTermsLink, '_blank')}
+            >
               서비스 이용약관
             </StyledListItem>
-            <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            <StyledListItem
+              rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}
+              onClick={() => window.open(privacyPolicyLink, '_blank')}
+            >
               개인 정보 처리 방침
             </StyledListItem>
-            <StyledListItem rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}>
+            <StyledListItem
+              rightIcon={<IcArrowRightLine color={theme.color.buttonNormal} />}
+              // onClick={() => window.open(openSourceLink, '_blank')}
+            >
               오픈소스 사용 정보
             </StyledListItem>
           </StyledMenuContainer>
