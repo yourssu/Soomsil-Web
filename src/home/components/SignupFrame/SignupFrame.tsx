198be03aa3730ef4f12cf952f31871b7b675c246
fecapark <fecapark@gmail.com>
2024-05-01 19:27:48 +0900

    feat: 로고 클릭시 홈으로 가는 기능 추가

--- src/home/components/SignupFrame/SignupFrame.tsx
+++ src/home/components/SignupFrame/SignupFrame.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import Logo from '@/assets/soomsil_v2_logo.svg';
 
 import {
@@ -11,9 +13,11 @@ interface SignupFrameProps {
 }
 
 export const SignupFrame = ({ children }: SignupFrameProps) => {
+  const navigate = useNavigate();
+
   return (
     <StyledSignupFrame>
-      <StyledSignupFrameLogo src={Logo} />
+      <StyledSignupFrameLogo src={Logo} onClick={() => navigate('/')} />
       <StyledSignupFrameContentContainer>{children}</StyledSignupFrameContentContainer>
     </StyledSignupFrame>
   );
