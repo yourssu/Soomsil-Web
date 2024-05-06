04a34991729245aa6d49a17dfb622623a8b7ec67
Hanna922 <hanna.urssu@gmail.com>
2024-05-06 22:28:20 +0900

    feat: create accessTokenUpdated CustomEvent

--- src/hooks/useIsLoggedIn.tsx
+++ src/hooks/useIsLoggedIn.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { useRecoilValue } from 'recoil';
 
 import { useGetUserData } from '@/home/hooks/useGetUserData';
@@ -5,11 +7,22 @@ import { UserState } from '@/home/recoil/UserState';
 import { api } from '@/service/TokenService';
 
 export const useIsLoggedIn = () => {
-  const isLoggedIn = api.getAccessToken() !== undefined;
+  const [isLoggedIn, setIsLoggedIn] = useState(api.getAccessToken() !== undefined);
   const currentUser = useRecoilValue(UserState);
   const { refetch } = useGetUserData();
 
   if (isLoggedIn && !currentUser) refetch();
+
+  useEffect(() => {
+    const tokenUpdated = (e: Event) => {
+      setIsLoggedIn((e as CustomEvent).detail.token !== undefined);
+    };
+
+    window.addEventListener('accessTokenUpdated', tokenUpdated);
+    return () => {
+      window.removeEventListener('accessTokenUpdated', tokenUpdated);
+    };
+  }, []);
 
   return isLoggedIn;
 };
