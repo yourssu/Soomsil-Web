7b49578a3727aaf5d5aaad7d1b9ee3b24af0926d
Hanna922 <hanna.urssu@gmail.com>
2024-05-06 22:26:55 +0900

    feat: add token management logic in basic apis file

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,4 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+import { refreshToken } from '@/home/apis/postRefreshToken';
+import { AuthErrorData } from '@/home/types/Auth.type';
+import { api } from '@/service/TokenService';
 
 export const soomsilClient = axios.create({
   baseURL: import.meta.env.VITE_API_SOOMSIL_URL,
@@ -14,3 +18,34 @@ export const authClient = axios.create({
     withCredentials: true,
   },
 });
+
+soomsilClient.interceptors.request.use((config) => {
+  if (config.headers && api.getRefreshToken()) {
+    config.headers.Authorization = `Bearer ${api.getAccessToken()}`;
+  }
+  return config;
+});
+
+const onFulfilled = (response: AxiosResponse) => response;
+
+const onRejected = async (error: AxiosError) => {
+  const originalConfig = error.config;
+  const data = error.response?.data as AuthErrorData;
+
+  if (originalConfig && error.response?.status === 401 && data?.error === 'Auth-002') {
+    try {
+      await refreshToken();
+      return soomsilClient({
+        ...originalConfig,
+        headers: {
+          Authorization: `Bearer ${api.getAccessToken()}`,
+        },
+      });
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+  return Promise.reject(error);
+};
+
+soomsilClient.interceptors.response.use(onFulfilled, onRejected);
