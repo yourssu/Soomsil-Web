69b5624eb7371c2f05dd83efe1f1a768a3fc1a09
Soomin Lee <sallybig@naver.com>
2024-05-20 01:25:06 +0900

    refactor: search 페이지 코드 개선 (#162)
    * refactor: search FallbackComponent 추상화 작업
    
    * refactor: ResultListItems -> ResultList 컴포넌트명 변경
    
    * refactor: useScrollObserve hook 분리
    
    * chore: ppussungError svg 추가
    
    * feat: 서버에러 FallbackComponent 제작
    
    * fix: 불필요한 코드 삭제
    
    * fix: StyledTextContainer 스타일 속성 추가
    
    * refactor: 검색 결과 없음 customErrorcode으로 리팩토링
    
    * refactor: navigation_options 상수화
    
    * refactor: 무한스크롤 코드 리팩토링
    
    * refactor: suspense TotalCount 컴포넌트 내부로 이동
    
    * refactor: YDS 제거 및 StyledBoxButton 추가
    
    * fix: navigate(0) -> navigate(-1)
    
    * fix: ResultListItem -> ResultList 폴더명 수정
    
    * refactor: customError 함수형으로 수정 및 네이밍 수정
    
    * fix: 이름 속성값 고정 -> 인자로 받는 것으로 변경
    
    * fix: FallbackComponent -> ErrorFallback rename
    
    * feat: NoResultFallback 컴포넌트 제작
    
    * fix: 숨실 -> 숨쉴 오타 수정
    
    * refactor: error 처리 방식 -> NoResultFallback 처리 방식 수정
    
    * fix: 불필요한 코드 삭제
    
    * fix: 불필요한 코드 삭제

--- src/search/components/TotalCount/TotalCount.tsx
+++ src/search/components/TotalCount/TotalCount.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from 'react';
+
 import { useSearchParams } from 'react-router-dom';
 
+import { Spacing } from '@/components/Spacing/Spacing';
 import { useGetSearch } from '@/search/hooks/useGetSearch';
 import { StyledTotalCount } from '@/search/pages/Search/Search.style';
+
+import NoResultFallback from '../FallbackComponent/NoResultFallback';
 
 export const TotalCount = () => {
   const [searchParams] = useSearchParams();
@@ -12,6 +17,13 @@ export const TotalCount = () => {
   });
 
   return (
-    <StyledTotalCount>{`${data.pages[0].totalCount}개의 검색 결과가 있습니다`}</StyledTotalCount>
+    <NoResultFallback
+      results={data?.pages[0].resultList}
+      fallback={<Spacing direction="vertical" size={21} />}
+    >
+      <Suspense fallback={<Spacing direction="vertical" size={21} />}>
+        <StyledTotalCount>{`${data.pages[0].totalCount}개의 검색 결과가 있습니다`}</StyledTotalCount>
+      </Suspense>
+    </NoResultFallback>
   );
 };
