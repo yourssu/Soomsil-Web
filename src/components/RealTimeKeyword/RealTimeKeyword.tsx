77c2715e0cd2798c2448d95345ab6659240ddfaa
심민성 <bighammer1753@gmail.com>
2024-05-21 16:40:04 +0900

    refactor: variantStyles constant 파일로 분리

--- src/components/RealTimeKeyword/RealTimeKeyword.tsx
+++ src/components/RealTimeKeyword/RealTimeKeyword.tsx
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom';
 
 import RealTimeKeywordImage from '@/assets/realTimeKeyword.webp';
 import {
+  variantStyles,
+  VariantType,
+} from '@/components/RealTimeKeyword/RealTimeKeyword.constant.ts';
+import {
   StyledContainer,
   StyledTitleContainer,
   StyledImage,
@@ -14,43 +18,9 @@ import {
   StyledListItemRanking,
   StyledListItemKeyword,
   StyledListItem,
-  RealTimeKeywordStyleProps,
 } from '@/components/RealTimeKeyword/RealTimeKeyword.style.ts';
 import { useGetRealTimeKeyword } from '@/search/hooks/useGetRealTimeKeyword.ts';
 import { formatDateTime } from '@/utils/formatDateTime.ts';
-
-type VariantType = 'home' | 'search';
-
-const variantStyles: { [key in VariantType]: RealTimeKeywordStyleProps } = {
-  home: {
-    $containerPadding: '1rem',
-    $containerWidth: '32.5rem',
-    $containerHeight: 'auto',
-    $titleContainerPadding: '0.3125rem 0.5rem',
-    $titleContainerMarginBottom: '0.5rem',
-    $updateDateTypo: 'caption2',
-    $columnCount: 2,
-    $keywordWidth: '7.375rem',
-    $imageWidth: '10.3125rem',
-    $imageHeight: '6.875rem',
-    $imageTop: '-1.125rem',
-    $imageRight: '0.9375rem',
-  },
-  search: {
-    $containerPadding: '1.25rem',
-    $containerWidth: '25rem',
-    $containerHeight: '38.7rem',
-    $titleContainerPadding: '0.5rem',
-    $titleContainerMarginBottom: '0.75rem',
-    $updateDateTypo: 'body3',
-    $columnCount: 1,
-    $keywordWidth: 'auto',
-    $imageWidth: '12.6875rem',
-    $imageHeight: '8.5rem',
-    $imageTop: '-3.125rem',
-    $imageRight: '0',
-  },
-};
 
 interface RealTimeKeywordProps {
   variant: VariantType;
