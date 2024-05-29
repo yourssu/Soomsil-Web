e08ce8814b1d4f331557f93beb7f4a923cb74f0a
nijuy <youjin6325@naver.com>
2024-05-29 23:10:22 +0900

    fix: `DialogState`의 persist 삭제

--- src/recoil/DialogState.ts
+++ src/recoil/DialogState.ts
@@ -2,10 +2,7 @@ import { atom } from 'recoil';
 
 import { DialogData } from '@/types/dialog.type';
 
-import { persistAtom } from './persistAtom';
-
 export const DialogState = atom<DialogData | null>({
   key: 'DialogState',
   default: null,
-  effects_UNSTABLE: [persistAtom],
 });
