e67a24ada5712632ca3539d4d8f24b04771c83e3
nijuy <youjin6325@naver.com>
2024-06-04 22:23:06 +0900

    refactor: `LoginDialog` handleClick 삭제
    - dialog 닫는 건 DialogClose로 감싸면 될 일

--- src/components/Dialog/LoginDialog/LoginDialog.tsx
+++ src/components/Dialog/LoginDialog/LoginDialog.tsx
@@ -1,38 +1,33 @@
 import { DialogClose } from '@radix-ui/react-dialog';
 import { BoxButton } from '@yourssu/design-system-react';
 import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
 
 import Ppussung from '@/assets/defaultProfile.png';
 import { ProfileSvg } from '@/components/ProfileSvg/ProfileSVG';
-import { DialogState } from '@/recoil/DialogState';
 
 import { StyledContainer, StyledButtonContainer, StyledTitle } from './LoginDialog.style';
 
 export const LoginDialog = () => {
-  const setDialog = useSetRecoilState(DialogState);
-
   const navigate = useNavigate();
-
-  const handleClick = () => {
-    setDialog(null);
-    navigate('/login');
-  };
 
   return (
     <StyledContainer>
       <ProfileSvg imageUrl={Ppussung} />
       <StyledTitle>{'로그인이 필요한 서비스입니다.\n로그인하시겠어요?'}</StyledTitle>
       <StyledButtonContainer>
-        <BoxButton
-          rounding={4}
-          size="large"
-          variant="filled"
-          width="10.75rem"
-          onClick={handleClick}
-        >
-          예
-        </BoxButton>
+        <DialogClose asChild>
+          <BoxButton
+            rounding={4}
+            size="large"
+            variant="filled"
+            width="10.75rem"
+            onClick={() => {
+              navigate('/login');
+            }}
+          >
+            예
+          </BoxButton>
+        </DialogClose>
         <DialogClose asChild>
           <BoxButton rounding={4} size="large" variant="line" width="10.75rem">
             아니오
