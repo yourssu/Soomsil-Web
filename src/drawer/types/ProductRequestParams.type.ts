0872aedbf4affafa4025e636549028c15077a166
seocylucky <seocylucky@github.com>
2024-06-06 02:12:43 +0900

    feat: ProductSearchRequestParams 타입 추가

--- src/drawer/types/ProductRequestParams.type.ts
+++ src/drawer/types/ProductRequestParams.type.ts
@@ -3,3 +3,9 @@ export interface ProductRequestParams {
   category?: string;
   page: number;
 }
+
+export interface ProductSearchRequestParams {
+  keyword?: string;
+  category?: string;
+  page: number;
+}
