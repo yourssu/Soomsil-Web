416f7d4ac1b4e64cc29608fe9088b7ca2a50dc21
solmee <kii5541@naver.com>
2024-06-12 15:30:00 +0900

    refactor: 기존 resetUserInfo으로 분리했던 코드를 하나로 통합

--- src/hooks/useResetUserInfo.ts
+++ src/hooks/useResetUserInfo.ts
@@ -4,18 +4,13 @@ import { LogInState } from '@/home/recoil/LogInState';
 import { UserState } from '@/home/recoil/UserState';
 import { api } from '@/service/TokenService';
 
-const resetUserInfo = (
-  resetLoginState: (value: boolean) => void,
-  resetUserState: (value: null) => void
-) => {
-  resetLoginState(false);
-  resetUserState(null);
-  api.logout();
-};
-
 export const useResetUserInfo = () => {
   const resetLoginState = useSetRecoilState(LogInState);
   const resetUserState = useSetRecoilState(UserState);
 
-  return () => resetUserInfo(resetLoginState, resetUserState);
+  return () => {
+    resetLoginState(false);
+    resetUserState(null);
+    api.logout();
+  };
 };
