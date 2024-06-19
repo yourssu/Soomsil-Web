8b0af1176b2cde92045e1104eb3952450b9a1249
solmee <kii5541@naver.com>
2024-06-20 00:42:04 +0900

    refactor: postWithdraw 로직을 mutation을 활용하여 분리 및 alert 대신 errorboundary로 수정

--- src/home/apis/postWithdraw.ts
+++ src/home/apis/postWithdraw.ts
@@ -1,21 +1,9 @@
-import { isAxiosError } from 'axios';
-
 import { authClient } from '@/apis';
-import { PostWithdrawResponse, AuthErrorData } from '@/home/types/Auth.type';
 import { api } from '@/service/TokenService';
 
-export const postWithdraw = async (): Promise<PostWithdrawResponse> => {
-  try {
-    await authClient.post(
-      '/auth/withdraw',
-      {},
-      {
-        headers: api.headers,
-      }
-    );
-    return { success: true };
-  } catch (error) {
-    if (isAxiosError<AuthErrorData>(error)) return { success: false, error };
-    return Promise.reject(error);
-  }
+export const postWithdraw = async (data: object) => {
+  const res = await authClient.post('/auth/withdraw', data, {
+    headers: api.headers,
+  });
+  return res.data;
 };
