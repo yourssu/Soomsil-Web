e6b2550bd9c07c17cb6f18965405d3e5d322ddeb
jonique98 <tnals9899@naver.com>
2024-06-21 01:43:55 +0900

    refactor: privateRoute에서 로그아웃 후 login 페이지로 이동하는 방식으로 변경

--- src/hooks/usePrivateRoute.tsx
+++ src/hooks/usePrivateRoute.tsx
@@ -1,13 +1,22 @@
 import { RouteProps, Navigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import { LogInState } from '@/home/recoil/LogInState';
+import { UserState } from '@/home/recoil/UserState';
+import { api } from '@/service/TokenService';
 
 export const usePrivateRoute = () => {
   const isLoggedIn = useRecoilValue(LogInState);
+  const accessToken = api.getAccessToken();
+
+  const setLogInState = useSetRecoilState(LogInState);
+  const setUserState = useSetRecoilState(UserState);
 
   const PrivateRoute = ({ element }: RouteProps): React.ReactNode => {
-    if (!isLoggedIn) {
+    if (!isLoggedIn || !accessToken) {
+      api.logout();
+      setUserState(null);
+      setLogInState(false);
       return <Navigate to="/login" />;
     }
     return element;
