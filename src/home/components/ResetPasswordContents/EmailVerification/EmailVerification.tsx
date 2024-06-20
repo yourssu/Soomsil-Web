cf852d9fb6d2f655517f69152f96fc216f61316b
solmee <kii5541@naver.com>
2024-06-20 22:37:56 +0900

    feat: 이메일인증 스텝에 이메일 인증 요청하기 api 연결하여 인증 메일 재전송 구현

--- src/home/components/ResetPasswordContents/EmailVerification/EmailVerification.tsx
+++ src/home/components/ResetPasswordContents/EmailVerification/EmailVerification.tsx
@@ -6,6 +6,8 @@ import {
   StyledTitleText,
 } from './EmailVerification.style';
 import { useSecondTimer } from '@/hooks/useSecondTimer';
+import { postAuthVerificationEmail } from '@/home/apis/authVerification';
+import { useFullEmail } from '@/hooks/useFullEmail';
 
 interface EmailVerificationProps {
   email: string;
@@ -14,13 +16,28 @@ interface EmailVerificationProps {
 
 export const EmailVerification = ({ email, onConfirm }: EmailVerificationProps) => {
   const { leftTime, resetTimer } = useSecondTimer(480);
+  const fullEmail = useFullEmail(email);
 
   const handleTimer = (seconds: number) => {
     const minutes = Math.floor(seconds / 60);
     const secs = seconds % 60;
     return `${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
   };
-  console.log(email);
+
+  const handleResendEmail = async () => {
+    resetTimer();
+
+    const response = await postAuthVerificationEmail({
+      email: fullEmail,
+      verificationType: 'PASSWORD',
+    });
+
+    if (response.error) {
+      console.error('이메일 전송 중 오류 발생');
+    } else {
+      console.log('인증 메일 재전송 완료');
+    }
+  };
 
   return (
     <>
@@ -30,7 +47,7 @@ export const EmailVerification = ({ email, onConfirm }: EmailVerificationProps) 
         새로운 비밀번호를 재설정해주세요.
       </StyledSubTitleText>
       <StyledTimer>{handleTimer(leftTime)}</StyledTimer>
-      <StyledButtonText onClick={resetTimer}>인증 메일 재전송</StyledButtonText>
+      <StyledButtonText onClick={handleResendEmail}>인증 메일 재전송</StyledButtonText>
       <BoxButton
         style={{ width: '100%' }}
         size="large"
