1e218f020a8099e43c9f5f4ace23b881c86af322
solmee <kii5541@naver.com>
2024-06-21 13:24:57 +0900

    fix: UserState 관련 코드 삭제

--- src/hooks/useResetUserInfo.ts
+++ src/hooks/useResetUserInfo.ts
@@ -1,16 +1,13 @@
 import { useSetRecoilState } from 'recoil';
 
 import { LogInState } from '@/home/recoil/LogInState';
-import { UserState } from '@/home/recoil/UserState';
 import { api } from '@/service/TokenService';
 
 export const useResetUserInfo = () => {
   const resetLoginState = useSetRecoilState(LogInState);
-  const resetUserState = useSetRecoilState(UserState);
 
   return () => {
     resetLoginState(false);
-    resetUserState(null);
     api.logout();
   };
 };
