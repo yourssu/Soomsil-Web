972005f487ee9a46789f98f9a581de838ee1f847
solmee <kii5541@naver.com>
2024-06-25 18:18:19 +0900

    fix: 비밀번호 재설정 완료 페이지 문구 변경

--- src/home/components/ResetPasswordContents/ResetPasswordSuccess/ResetPasswordSuccess.tsx
+++ src/home/components/ResetPasswordContents/ResetPasswordSuccess/ResetPasswordSuccess.tsx
@@ -8,8 +8,7 @@ export const ResetPasswordSuccess = () => {
       <StyledTitleText>비밀번호 재설정</StyledTitleText>
       <StyledSubTitleText>
         비밀번호 재설정이 완료되었습니다.
-        <br />
-        보안을 위해 재로그인해주세요.
+        <br />새 비밀번호로 로그인해주세요.
       </StyledSubTitleText>
       <StyledLink to="/login">
         <BoxButton style={{ width: '100%' }} size="large" variant="filled" rounding={8}>
