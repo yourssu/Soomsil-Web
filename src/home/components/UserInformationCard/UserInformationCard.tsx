66c603aee234cfd78c0f2e96f6db65fb2fe96038
이유진 <youjin6325@naver.com>
2024-06-26 00:07:48 +0900

    feat: `UserInformationCard`에 현재 로그인 중인 사용자 정보 넣기 (#232)
    * feat: `UserInformationCard`에 현재 로그인 중인 사용자 정보 넣기
    
    - 프로필 수정 기능에 접근하지 못하게 주석 처리 해둠
    
    * chore: `UserInformationCard` 주석 범위 수정

--- src/home/components/UserInformationCard/UserInformationCard.tsx
+++ src/home/components/UserInformationCard/UserInformationCard.tsx
@@ -1,88 +1,88 @@
-import { useState } from 'react';
+// TODO: 프로필 편집 관련 코드 임시 주석 처리
+// import { useState } from 'react';
 
-import { BoxButton, IcSettingLine, SimpleTextField } from '@yourssu/design-system-react';
-import { useTheme } from 'styled-components';
+// import { BoxButton, SimpleTextField, IcSettingLine } from '@yourssu/design-system-react';
+// import { useTheme } from 'styled-components';
 
-import Ppussung from '@/assets/defaultProfile.png';
 import { ProfileSvg } from '@/components/ProfileSvg/ProfileSVG';
+import { useGetUserData } from '@/home/hooks/useGetUserData';
 
 import {
-  StyledButtonContainer,
+  // StyledButtonContainer,
   StyledContainer,
   StyledInformationContainer,
-  StyledSettingButton,
+  // StyledSettingButton,
   StyledUserIconContainer,
   StyledUserMail,
   StyledUserNickname,
 } from './UserInformationCard.style';
 
-const Dummy = {
-  name: '김뿌슝',
-  mail: 'ppussung@yourssu.com',
-  image: Ppussung,
-};
-
 export const UserInformationCard = () => {
-  const [activeEditMode, setAcitveEditMode] = useState(false);
-  const [nickname, setNickname] = useState(Dummy.name);
-  const theme = useTheme();
+  const { data: currentUser } = useGetUserData();
+  // const [activeEditMode, setAcitveEditMode] = useState(false);
+  // const [nickname, setNickname] = useState<string>('');
+  // const theme = useTheme();
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNickname(event.target.value);
-  };
+  // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setNickname(event.target.value);
+  // };
 
   return (
     <StyledContainer>
-      <StyledInformationContainer>
-        <StyledUserIconContainer>
-          <ProfileSvg imageUrl={Dummy.image} />
-          {activeEditMode && (
-            <StyledSettingButton>
-              <IcSettingLine color={theme.color.buttonNormal} size="1rem" />
-            </StyledSettingButton>
-          )}
-        </StyledUserIconContainer>
-        <StyledUserNickname>{Dummy.name}</StyledUserNickname>
-        <StyledUserMail>{Dummy.mail}</StyledUserMail>
-      </StyledInformationContainer>
-      {activeEditMode ? (
+      {currentUser && (
         <>
-          <SimpleTextField
-            fieldLabel="닉네임"
-            placeholder="뿌슝이"
-            width="100%"
-            onClickClearButton={() => setNickname('')}
-            value={nickname}
-            onChange={handleInputChange}
-          />
-          <StyledButtonContainer>
-            <BoxButton size="medium" variant="filled" rounding={4} width="100%">
-              저장
-            </BoxButton>
+          <StyledInformationContainer>
+            <StyledUserIconContainer>
+              <ProfileSvg imageUrl={currentUser.profileImage.midUrl} />
+              {/* {activeEditMode && (
+                <StyledSettingButton>
+                  <IcSettingLine color={theme.color.buttonNormal} size="1rem" />
+                </StyledSettingButton>
+              )} */}
+            </StyledUserIconContainer>
+            <StyledUserNickname>{currentUser.nickName}</StyledUserNickname>
+            <StyledUserMail>{currentUser.email}</StyledUserMail>
+          </StyledInformationContainer>
+          {/* {activeEditMode ? (
+            <>
+              <SimpleTextField
+                fieldLabel="닉네임"
+                placeholder="뿌슝이"
+                width="100%"
+                onClickClearButton={() => setNickname('')}
+                value={nickname}
+                onChange={handleInputChange}
+              />
+              <StyledButtonContainer>
+                <BoxButton size="medium" variant="filled" rounding={4} width="100%">
+                  저장
+                </BoxButton>
+                <BoxButton
+                  size="medium"
+                  variant="line"
+                  rounding={4}
+                  width="100%"
+                  onClick={() => setAcitveEditMode(false)}
+                >
+                  취소
+                </BoxButton>
+              </StyledButtonContainer>
+            </>
+          ) : (
             <BoxButton
               size="medium"
               variant="line"
               rounding={4}
               width="100%"
-              onClick={() => setAcitveEditMode(false)}
+              onClick={() => {
+                setAcitveEditMode(true);
+                setNickname(currentUser.nickName);
+              }}
             >
-              취소
+              프로필 편집
             </BoxButton>
-          </StyledButtonContainer>
+          )} */}
         </>
-      ) : (
-        <BoxButton
-          size="medium"
-          variant="line"
-          rounding={4}
-          width="100%"
-          onClick={() => {
-            setAcitveEditMode(true);
-            setNickname(Dummy.name);
-          }}
-        >
-          프로필 편집
-        </BoxButton>
       )}
     </StyledContainer>
   );
