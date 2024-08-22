db31ed49502ac035f056a4734800b04d694fef35
nijuy <youjin6325@naver.com>
2024-08-23 05:05:14 +0900

    refactor: `MoreProductSection`의 `StyledArrowButton` 삭제
    - img -> Link로 img를 감싼 형태로 수정

--- src/drawer/components/ServiceDetailContents/MoreProductSection/MoreProductSection.tsx
+++ src/drawer/components/ServiceDetailContents/MoreProductSection/MoreProductSection.tsx
@@ -1,32 +1,21 @@
-import { useNavigate } from 'react-router-dom';
-
 import IcRightArrow from '@/drawer/assets/ic_right_arrow.svg';
 import { SmallDrawerCard } from '@/drawer/components/DrawerCard/SmallDrawerCard';
 import { useGetProductByProvider } from '@/drawer/hooks/useGetProductByProvider';
 
-import {
-  StyledArrowButton,
-  StyledContainer,
-  StyledMoreProductSection,
-} from './MoreProductSection.style';
+import { StyledContainer, StyledLink, StyledMoreProductSection } from './MoreProductSection.style';
 
 export const MoreProductSection = ({ providerId }: { providerId: string }) => {
   const { data } = useGetProductByProvider({ providerId });
   const { providerName, products } = data.pages[0];
-
-  const navigate = useNavigate();
 
   if (products.length === 0) return null;
   return (
     <StyledMoreProductSection>
       <StyledContainer>
         {providerName}의 서비스 더보기
-        <StyledArrowButton
-          src={IcRightArrow}
-          onClick={() => {
-            navigate(`/drawer/${providerId}`);
-          }}
-        />
+        <StyledLink to={`/drawer/${providerId}`} title={'more-production-link'}>
+          <img src={IcRightArrow} alt={'right-arrow-icon'} />
+        </StyledLink>
       </StyledContainer>
       {products.slice(0, 5).map(({ productTitle, count, productNo, mainImage, isBookmarked }) => (
         <SmallDrawerCard
