3d641205104e97e2cc30f73e0de05aabff688665
nijuy <youjin6325@naver.com>
2024-08-24 18:45:53 +0900

    fix: 서랍장 검색 결과 없을 때 `서비스 등록하기` 버튼 핸들러 추가

--- src/drawer/components/EmptyScreen/EmptyScreen.tsx
+++ src/drawer/components/EmptyScreen/EmptyScreen.tsx
@@ -1,7 +1,11 @@
 import { BoxButton } from '@yourssu/design-system-react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 import NotIllust from '@/drawer/assets/noResultDrawer.png';
 import { NOT_FOUND_TEXT } from '@/drawer/constants/empty.constant';
+import { LogInState } from '@/home/recoil/LogInState';
+import { DialogState } from '@/recoil/DialogState';
 
 import {
   StyledNotContainer,
@@ -17,16 +21,34 @@ interface EmptyScreenProps {
 export const EmptyScreen = ({ type }: EmptyScreenProps) => {
   const { boldText, subText } = NOT_FOUND_TEXT[type];
 
+  const setDialog = useSetRecoilState(DialogState);
+  const isLoggedIn = useRecoilValue(LogInState);
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    if (isLoggedIn) {
+      navigate('/drawer/register');
+      return;
+    }
+    setDialog({ open: true, type: 'login' });
+  };
+
   return (
     <StyledNotContainer>
       <StyledNotTextContainer>
         <StyledNotTextBold>{boldText}</StyledNotTextBold>
         <div>{subText}</div>
       </StyledNotTextContainer>
-      <StyledNotImg src={NotIllust} />
+      <StyledNotImg src={NotIllust} alt="no-result-ppussung" />
 
       {(type === 'SEARCH' || type === 'MYDRAWER') && (
-        <BoxButton size="small" variant="filled" rounding={4}>
+        <BoxButton
+          size="small"
+          variant="filled"
+          rounding={4}
+          type="button"
+          onClick={handleClickButton}
+        >
           서비스 등록하기
         </BoxButton>
       )}
