c775779ee82106e4cf825b8c55421626cf4a293d
2wndrhs <2wndrhs@gmail.com>
2024-08-24 16:35:17 +0900

    refactor: 필요없는 쿼리 옵션 제거

--- src/search/apis/getRealTimeKeyword.ts
+++ src/search/apis/getRealTimeKeyword.ts
@@ -2,7 +2,7 @@ import { searchClient } from '@/apis';
 
 import { RealTimeKeywordData } from '../types/realTimeKeyword.type';
 
-export const getRealTimeKeyword = () => {
-  const response = searchClient.get<RealTimeKeywordData>('search/topQueries');
-  return response;
+export const getRealTimeKeyword = async () => {
+  const { data } = await searchClient.get<RealTimeKeywordData>('search/topQueries');
+  return data;
 };
