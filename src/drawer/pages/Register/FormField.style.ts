0416bcbf867dcbcd48aca847aa63442584b32c71
2wndrhs <2wndrhs@gmail.com>
2024-08-30 02:16:06 +0900

    feat: `FormField` 컴포넌트 구현

---
+++ src/drawer/pages/Register/FormField.style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+interface IsWarnedProps {
+  $isWarned: boolean;
+}
+
+export const StyledFieldContainer = styled.div`
+  display: flex;
+  gap: 12.75rem;
+`;
+
+export const StyledLabelContainer = styled.div`
+  display: flex;
+  white-space: nowrap;
+  flex-direction: column;
+  gap: 0.625rem;
+`;
+
+export const StyledLabel = styled.label<IsWarnedProps>`
+  ${({ theme }) => theme.typo.subtitle6}
+  color: ${({ theme, $isWarned }) =>
+    $isWarned ? theme.color.textWarned : theme.color.textPrimary};
+`;
+
+export const StyledLabelHint = styled.span<IsWarnedProps>`
+  ${({ theme }) => theme.typo.caption2}
+  color: ${({ theme, $isWarned }) =>
+    $isWarned ? theme.color.textWarned : theme.color.textPrimary};
+`;
+
+export const StyledControlContainer = styled.div`
+  flex-grow: 1;
+`;
+
+export const StyledControlMessage = styled.div<IsWarnedProps>`
+  ${({ theme }) => theme.typo.caption2}
+  color: ${({ theme, $isWarned }) =>
+    $isWarned ? theme.color.textWarned : theme.color.textPrimary};
+  text-align: right;
+`;
---
+++ src/drawer/pages/Register/FormField.tsx
@@ -0,0 +1,96 @@
+import React, { createContext, ReactElement, ReactNode, useContext } from 'react';
+
+import { RegisterOptions, useFormContext, ValidationValueMessage } from 'react-hook-form';
+
+import {
+  StyledControlContainer,
+  StyledControlMessage,
+  StyledFieldContainer,
+  StyledLabel,
+  StyledLabelContainer,
+  StyledLabelHint,
+} from './FormField.style';
+
+interface FormFieldProps {
+  children: ReactNode;
+  name: string;
+  registerOption: RegisterOptions;
+}
+
+interface FieldLabelProps {
+  children: ReactNode;
+  hint: string;
+}
+
+interface FieldControlProps {
+  children: ReactElement;
+}
+
+const FormFieldContext = createContext<Omit<FormFieldProps, 'children'>>({
+  name: '',
+  registerOption: {},
+});
+
+export const FormField = ({ children, name, registerOption }: FormFieldProps) => {
+  return (
+    <FormFieldContext.Provider value={{ name, registerOption }}>
+      <StyledFieldContainer>{children}</StyledFieldContainer>
+    </FormFieldContext.Provider>
+  );
+};
+
+const FieldLabel = ({ children, hint }: FieldLabelProps) => {
+  const {
+    formState: { errors },
+  } = useFormContext();
+  const { name, registerOption } = useContext(FormFieldContext);
+
+  return (
+    <StyledLabelContainer>
+      <StyledLabel htmlFor={name} $isWarned={!!errors[name]}>
+        {children}
+        {registerOption.required && ' *'}
+      </StyledLabel>
+      <StyledLabelHint $isWarned={!!errors[name]}>({hint})</StyledLabelHint>
+    </StyledLabelContainer>
+  );
+};
+
+const FieldControl = ({ children }: FieldControlProps) => {
+  const {
+    register,
+    formState: { errors },
+    watch,
+  } = useFormContext();
+  const { name, registerOption } = useContext(FormFieldContext);
+
+  const watchField = watch(name);
+
+  const getControlMessage = () => {
+    const errorMessage = errors[name]?.message;
+    if (errorMessage) {
+      return String(errorMessage);
+    }
+
+    if (registerOption.maxLength) {
+      const maxLength = registerOption.maxLength as ValidationValueMessage<number>;
+      return `${watchField.length}/${maxLength.value}`;
+    }
+
+    return '';
+  };
+
+  return (
+    <StyledControlContainer>
+      {React.cloneElement(children, {
+        $isWarned: !!errors[name],
+        id: name,
+        ...register(name, registerOption),
+      })}
+      <StyledControlMessage $isWarned={!!errors[name]}>{getControlMessage()}</StyledControlMessage>
+    </StyledControlContainer>
+  );
+};
+
+FormField.Label = FieldLabel;
+FormField.Control = FieldControl;
