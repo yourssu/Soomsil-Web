a9dc588cd03098848def4f2ce134b342fb375e89
seocylucky <seocylucky@github.com>
2024-09-01 16:12:44 +0900

    feat: Empty PROVIDER 부분 멘트 수정

--- src/drawer/constants/empty.constant.ts
+++ src/drawer/constants/empty.constant.ts
@@ -15,7 +15,7 @@ export const NOT_FOUND_TEXT: Record<
     subText: '새로운 서비스를 등록해보세요.',
   },
   PROVIDER: {
-    boldText: '아직 해당 카테고리에 업로드한 서비스가 없습니다.',
-    subText: '',
+    boldText: '현재 해당 카테고리에 등록된 서비스가 없습니다.',
+    subText: '다른 카테고리를 확인해보세요!',
   },
 };
