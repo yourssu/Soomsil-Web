ebf21b024e899a912479b5274c15385857627bc9
nijuy <youjin6325@naver.com>
2024-09-02 20:14:30 +0900

    refactor: `realTimeDate` 객체 벗기기

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -1,11 +1,11 @@
 export const formatDateTime = (basedTime: string): string => {
   const dateObject = new Date(basedTime);
-  const realTimeDate = {
-    year: dateObject.getFullYear(),
-    month: (dateObject.getMonth() + 1).toString(),
-    day: dateObject.getDate().toString(),
-    hour: dateObject.getHours().toString().padStart(2, '0'),
-    minute: dateObject.getMinutes().toString().padStart(2, '0'),
-  };
-  return `${realTimeDate.year}년 ${realTimeDate.month}월 ${realTimeDate.day}일 ${realTimeDate.hour}:${realTimeDate.minute}`;
+
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1;
+  const day = dateObject.getDate();
+  const hour = dateObject.getHours().toString().padStart(2, '0');
+  const minute = dateObject.getMinutes().toString().padStart(2, '0');
+
+  return `${year}년 ${month}월 ${day}일 ${hour}:${minute}`;
 };
