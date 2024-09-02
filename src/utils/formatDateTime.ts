54cf08004c9e3b9beff26bd1c27c09334bf0068d
nijuy <youjin6325@naver.com>
2024-09-02 19:23:22 +0900

    fix: day, hour, minute에 붙는 + 1 삭제

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -3,9 +3,9 @@ export const formatDateTime = (basedTime: string): string => {
   const realTimeDate = {
     year: dateObject.getFullYear(),
     month: (dateObject.getMonth() + 1).toString().padStart(2, '0'),
-    day: (dateObject.getDate() + 1).toString().padStart(2, '0'),
-    hour: (dateObject.getHours() + 1).toString().padStart(2, '0'),
-    minute: (dateObject.getMinutes() + 1).toString().padStart(2, '0'),
+    day: dateObject.getDate().toString().padStart(2, '0'),
+    hour: dateObject.getHours().toString().padStart(2, '0'),
+    minute: dateObject.getMinutes().toString().padStart(2, '0'),
   };
   return `${realTimeDate.year}.${realTimeDate.month}.${realTimeDate.day} ${realTimeDate.hour}:${realTimeDate.minute}`;
 };
