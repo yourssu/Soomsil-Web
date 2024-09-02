5d89b8cdce4c5ef0a51eb67c2e7628b1f6b3d7dc
nijuy <youjin6325@naver.com>
2024-09-02 20:07:39 +0900

    refactor: 앱과 날짜 형식 통일
    - month/day의 padStart 삭제
    - 리턴 값에 `년``월``일` 추가

--- src/utils/formatDateTime.ts
+++ src/utils/formatDateTime.ts
@@ -2,10 +2,10 @@ export const formatDateTime = (basedTime: string): string => {
   const dateObject = new Date(basedTime);
   const realTimeDate = {
     year: dateObject.getFullYear(),
-    month: (dateObject.getMonth() + 1).toString().padStart(2, '0'),
-    day: dateObject.getDate().toString().padStart(2, '0'),
+    month: (dateObject.getMonth() + 1).toString(),
+    day: dateObject.getDate().toString(),
     hour: dateObject.getHours().toString().padStart(2, '0'),
     minute: dateObject.getMinutes().toString().padStart(2, '0'),
   };
-  return `${realTimeDate.year}.${realTimeDate.month}.${realTimeDate.day} ${realTimeDate.hour}:${realTimeDate.minute}`;
+  return `${realTimeDate.year}년 ${realTimeDate.month}월 ${realTimeDate.day}일 ${realTimeDate.hour}:${realTimeDate.minute}`;
 };
