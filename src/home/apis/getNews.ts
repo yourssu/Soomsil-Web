0d3c81e28292d460440758a7d82c2471075c8ced
nijuy <youjin6325@naver.com>
2024-12-18 18:28:43 +0900

    refactor: `getNews` params 객체 추가가

--- src/home/apis/getNews.ts
+++ src/home/apis/getNews.ts
@@ -2,6 +2,10 @@ import { searchClient } from '@/apis';
 import { SsuNews } from '@/home/types/news.type';
 
 export const getNews = async (): Promise<SsuNews> => {
-  const { data } = await searchClient.get('/search/news?platform=WEB');
+  const { data } = await searchClient.get('/search/news', {
+    params: {
+      platform: 'WEB',
+    },
+  });
   return data;
 };
